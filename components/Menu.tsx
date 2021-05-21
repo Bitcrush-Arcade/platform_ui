dc06f1b3af2321b2c0ba077a17ff6b0238578dc5
jmanywhere <josmfernandez@gmail.com>
2021-05-21 13:21:58 -0600

    WIP menu style update

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,5 +1,7 @@
 // React imports
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
+// Next
+import { useRouter } from 'next/router'
 // Material imports
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
@@ -13,33 +15,35 @@ import ListItemText from '@material-ui/core/ListItemText'
 // Icons
 import HomeIcon from '@material-ui/icons/Home';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import GameIcon from '@material-ui/icons/VideogameAsset';
 
 
 const Menu = () => {
     
+    const router = useRouter()
     const [open, setOpen] = useState<boolean>(true)
-
     const css = useStyles({ open })
 
     const linkArray = [
-        { name: 'Home', icon: <HomeIcon color="secondary"/>, url_link: '/' },
-        { name: 'Intergalactic Trade', icon: null, url_link: '/' },
-        { name: 'Warp Speed', icon: null, url_link: '/' },
-        { name: 'Galactic Mining', icon: null, url_link: '/' },
-        { name: 'GAMES', icon: null, url_link: '/' },
-        { name: 'MORE', icon: null, subMenu: [ { name: 'GitHub', icon: <GitHubIcon color="secondary"/>, url_link: 'https://www.github.com'} ] },
+        { name: 'Home', icon: <HomeIcon color="inherit"/>, url_link: '/' },
+        { name: 'Intergalactic Trade', icon: null, url_link: '/trade' },
+        { name: 'Warp Speed', icon: null, url_link: '/warp' },
+        { name: 'Galactic Mining', icon: null, url_link: '/mining' },
+        { name: 'GAMES', icon: <GameIcon/>, url_link: '/games' },
+        { name: 'MORE', icon: null, subMenu: [ { name: 'GitHub', icon: <GitHubIcon color="inherit"/>, url_link: 'https://www.github.com'} ] },
     ]
 
     const linkItems = linkArray.map( link => {
         const { name, icon, url_link, subMenu } = link
         const [ openSubMenu, setOpenSubMenu ] = useState<boolean>(false)
         const click = () =>  subMenu && setOpenSubMenu( p => !p )
-        return(<>
-            <ListItem key={`nav-menu-item-${name}`} button={subMenu ? true: undefined} onClick={ click }>
-                <ListItemIcon>
+        const selected = url_link == router.pathname
+        return(<Fragment key={`nav-menu-item-${name}`} >
+            <ListItem button={subMenu ? true: undefined} onClick={ click }>
+                <ListItemIcon className={ selected && css.selectedIcon }>
                     {icon}
                 </ListItemIcon>
-                <ListItemText primary={name} primaryTypographyProps={{ noWrap: true }}/>
+                <ListItemText primary={name} primaryTypographyProps={{ noWrap: true, color: selected ? 'primary' : 'textPrimary' }}/>
             </ListItem>
             {subMenu && <Collapse in={openSubMenu}>
                 <List dense className={css.subList}>
@@ -54,12 +58,12 @@ const Menu = () => {
                     })}
                 </List>
             </Collapse>}
-        </>)
+        </Fragment>)
     })
 
     return <>
       <Drawer open={open} variant="permanent" className={ `${css.drawerContainer}` } PaperProps={{ className: css.drawer}}>
-        <Paper className={ `${css.drawerContainer} ${css.paper}` }>
+        <Paper className={ `${css.drawerContainer} ${css.paper}` } square>
             <List>
                 {linkItems}
             </List>
@@ -85,11 +89,22 @@ const useStyles = makeStyles( (theme: Theme) => createStyles({
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
           }),
+        overflowX: 'hidden',
         backgroundColor: 'rgb(23,24,54)',
-        color:  'white'
+        color: 'white',
+        paddingLeft: theme.spacing(1),
+        paddingTop: theme.spacing(1),
+        borderTopRightRadius: theme.spacing(3),
+        borderBottomRightRadius: theme.spacing(3),
+        borderColor: 'rgb(34,35,63)',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderLeftStyle: 'none',
     },
     subList: {
         backgroundColor: 'rgba(23,24,54, 0.5)'
+    },
+    selectedIcon:{
+        color: theme.palette.primary.main
     }
-
 }))
