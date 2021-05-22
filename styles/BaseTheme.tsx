85a80b8c93494d2e7f1d41aae162544f05b16dc6
jmanywhere <josmfernandez@gmail.com>
2021-05-22 10:42:51 -0600

    added deepPurple and teal

--- styles/BaseTheme.tsx
+++ styles/BaseTheme.tsx
@@ -1,12 +1,16 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/deepPurple'
+// import purple from '@material-ui/core/colors/purple'
+import teal from '@material-ui/core/colors/teal'
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: 'rgb(174,82,227)', // Purple
+      dark: deepPurple['A700']
     },
     secondary: {
-      main: 'rgb(43,216,180)', // Specified Black
+      main: teal['A400'],
     },
     background: {
       default: 'rgb(12,15,32)',
