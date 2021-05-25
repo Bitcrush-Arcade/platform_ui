a0fea9a5c5e318d2b843607e555d2d90d2482661
jmanywhere <josmfernandez@gmail.com>
2021-05-25 16:44:48 -0600

    prettify styling

--- components/pools/PoolCard.tsx
+++ components/pools/PoolCard.tsx
@@ -1,4 +1,5 @@
 // Material
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
 import Divider from '@material-ui/core/Divider'
@@ -13,22 +14,22 @@ import { currencyFormat } from 'utils/text/text'
 const PoolCard = (props: PoolCardProps) => {
 
   const { color, stakedInfo, rewardInfo, action1Title, action2Title, firstAction, secondAction, action2Color } = props
-
+  const css = useStyles({})
   const action1Width = action2Title ? 200 : '100%'
   const action1Grid = action2Title ? 'auto' : 12
-  return <Card background="light" style={{ minWidth: 300, maxWidth: '100%' }}>
-    <CardContent style={{ padding: 32 }}>
-      <Grid container justify="space-between" >
-        <Grid item xs={7}>
-          <Typography>
+  return <Card background="light" className={ css.main } >
+    <CardContent className={ css.content } >
+      <Grid container justify="space-between" direction="row-reverse" >
+        <Grid item>
+          {props.icon || 'ICON'}
+        </Grid>
+        <Grid item>
+          <Typography variant="h4" component="h2" className={css.title} >
             {props.title}
           </Typography>
           <InfoMoney color={rewardInfo?.color || color}
             {...rewardInfo}
           />
-        </Grid>
-        <Grid item>
-          {props.icon || 'ICON'}
         </Grid>
       </Grid>
       <Divider orientation="horizontal" style={{marginBottom: 8, marginTop: 8}}/>
@@ -36,8 +37,8 @@ const PoolCard = (props: PoolCardProps) => {
         {...stakedInfo}
       />
     </CardContent>
-    <CardActions style={{ padding: 24 }}>
-      <Grid container justify="space-between">
+    <CardActions className={ css.actions } >
+      <Grid container spacing={1} justify="space-between">
         <Grid item xs={action1Grid}>
           <Button color={color} width={action1Width} onClick={firstAction}>
             {action1Title}
@@ -78,11 +79,57 @@ type InfoStakeProps = {
 
 const InfoMoney = ( props: InfoStakeProps ) => {
   const { title, subtitle, currency, amount, color } = props
-  return <Typography variant="caption" color="textSecondary" style={{ fontSize: 10}}>
-    {title}
-    <Typography color={color || 'primary'}>
-      {currency}&nbsp;{currencyFormat(amount, { decimalsToShow: 4})}
+  const css = useMoneyStyles({})
+  return <>
+    <Typography variant="body2" color="textSecondary" className={ css.general }>
+      {title}
     </Typography>
-    {subtitle}
-  </Typography>
+    <Typography color={color || 'primary'} display="inline" className={ css.currency }>
+      {currency}
+      <Typography className={ css.value } display="inline">
+        {currencyFormat(amount, { decimalsToShow: 4})}
+      </Typography>
+    </Typography>
+    <Typography variant="body2" color="textSecondary" display="block" className={ css.general }>
+      {subtitle}
+    </Typography>
+  </>
 }
+
+const useStyles = makeStyles( (theme: Theme) => createStyles({
+  actions:{
+    padding: theme.spacing(4),
+    paddingTop: 0,
+  },
+  content:{
+    padding: theme.spacing(4),
+  },
+  main:{
+    minWidth: 250,
+    width: '100%',
+    maxWidth: 560,
+  },
+  title:{
+    fontWeight: 600,
+    textTransform: 'uppercase',
+    marginBottom: theme.spacing(4),
+    [theme.breakpoints.down('sm')]:{
+      marginBottom: theme.spacing(2),
+      fontSize: theme.typography.h5.fontSize
+    }
+  }
+}))
+const useMoneyStyles = makeStyles( (theme: Theme) => createStyles({
+  currency:{
+    fontWeight: 500,
+    fontSize: theme.typography.h5.fontSize,
+  },
+  value:{
+    fontSize: theme.typography.h4.fontSize,
+    fontWeight: 500,
+    paddingLeft: theme.spacing(1)
+  },
+  general:{
+    opacity: 0.75,
+  },
+}))
