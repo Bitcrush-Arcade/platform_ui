c0aeec5a078b728262cc0b9d255a2802634c18a0
jmanywhere <josmfernandez@gmail.com>
2021-05-26 13:23:37 -0600

    added disabled and tweaked names

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 // Material imports
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
 import Collapse from '@material-ui/core/Collapse'
 import Drawer from '@material-ui/core/Drawer'
 import Paper from '@material-ui/core/Paper'
@@ -13,10 +12,10 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
+import Typography from '@material-ui/core/Typography'
 // Icons
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import GameIcon from '@material-ui/icons/VideogameAsset';
 import HomeIcon from '@material-ui/icons/Home';
 // BitCrush
 import ArcadeIcon from 'components/svg/ArcadeIcon'
@@ -33,21 +32,21 @@ const Menu = ( props: { open: boolean, toggleOpen: () => void }) => {
     const router = useRouter()
     const css = useStyles({ open })
 
-    const linkArray = [
+    const linkArray: Array<LinkItem> = [
         { name: 'Home', icon: <HomeIcon color="inherit"/>, url_link: '/' },
-        { name: 'Intergalactic Trade', icon: <TradeIcon/>, url_link: '/trade' },
-        { name: 'Warp Speed', icon: <WarpIcon/>, url_link: '/warp' },
-        { name: 'Galactic Mining', icon: <UfoIcon/>, url_link: '/mining' },
-        { name: 'GAMES', icon: <ArcadeIcon/>, url_link: '/games' },
-        { name: `Recharging`, icon: <RechargeIcon/>, url_link: '/games' },
-        { name: `Crush n'Burn Lottery`, icon: <RocketIcon/>, url_link: '/games' },
-        { name: `NFTs`, icon: <Ufo2Icon/>, url_link: '/games' },
-        { name: `Black Hole Referral`, icon: <BlackHoleIcon/>, url_link: '/games' },
+        { name: 'Intergalactic Trade', icon: <TradeIcon/>, url_link: '/trade', disabled: true },
+        { name: 'Warp Speed', icon: <WarpIcon/>, url_link: '/warp', disabled: true },
+        { name: 'Galactic Mining', icon: <UfoIcon/>, url_link: '/mining', disabled: true },
+        { name: 'ARCADE', icon: <ArcadeIcon/>, url_link: '/games' },
+        { name: `Recharging`, icon: <RechargeIcon/>, url_link: '/recharge', disabled: true },
+        { name: `Crush n'Burn Lottery`, icon: <RocketIcon/>, url_link: '/lottery', disabled: true },
+        { name: `NFTs`, icon: <Ufo2Icon/>, url_link: '/nft', disabled: true },
+        { name: `Black Hodle Referral`, icon: <BlackHoleIcon/>, url_link: '/referral', disabled: true },
         { name: 'MORE', icon: null, subMenu: [ { name: 'GitHub', icon: <GitHubIcon color="inherit"/>, url_link: 'https://www.github.com'} ] },
     ]
 
     const linkItems = linkArray.map( link => {
-        const { name, icon, url_link, subMenu } = link
+        const { name, icon, url_link, subMenu, disabled } = link
         const [ openSubMenu, setOpenSubMenu ] = useState<boolean>(false)
         const click = () => {
             subMenu && setOpenSubMenu( p => !p )
@@ -55,22 +54,23 @@ const Menu = ( props: { open: boolean, toggleOpen: () => void }) => {
         }
         const selected = url_link == router.pathname
         const mainColor = selected ? 'primary' :
-            subMenu ? 'secondary' : 'textSecondary'
+            subMenu ? 'secondary' : undefined
+        const component = disabled ? 'li' :
+            url_link ? 'a' : 'button'
         const listItem = <Fragment key={`nav-menu-item-${name}`} >
             <ListItem 
-                button={subMenu || url_link ? true: undefined}
-                onClick={ click } component={url_link ? 'a' : 'button'}  href={url_link}
+                button={subMenu || url_link && !disabled ? true: undefined}
+                onClick={ click } component={component}  href={url_link}
                 className={ `${ selected ? css.selectedItem : ''} ${css.baseItem}`}
             >
                 <ListItemIcon className={ `${selected ? css.selectedIcon : css.baseIcon } ${css.listIcon}` }>
                     {icon}
                 </ListItemIcon>
                 <ListItemText
-                    primary={subMenu
-                        ? <>
-                            {name} {<ExpandMoreIcon fontSize="inherit" style={{transform: openSubMenu ? 'rotate( 180deg )' : undefined}}/>}
-                          </> 
-                        : name}
+                    primary={<>
+                        {name} {subMenu && <ExpandMoreIcon fontSize="inherit" style={{transform: openSubMenu ? 'rotate( 180deg )' : undefined}}/>}
+                        {disabled && <Typography variant="caption" className={ css.disabled }>(coming soon)</Typography>}
+                    </>}
                     primaryTypographyProps={{ noWrap: true, color: mainColor, variant: 'body1', className: css.menuTextPrimary  }}
                 />
             </ListItem>
@@ -159,14 +159,34 @@ const useStyles = makeStyles( (theme: Theme) => createStyles({
     baseItem:{
         borderTopLeftRadius: theme.spacing(2),
         borderBottomLeftRadius: theme.spacing(2),
+        marginTop: theme.spacing(0.5)
     },
     listIcon:{
         minWidth: theme.spacing(6),
     },
     menuTextPrimary:{
         fontSize: 10,
+        color: theme.palette.grey[200],
     },
     menuTextSecondary:{
         fontSize: 10,
+    },
+    disabled:{
+        fontSize: 8,
+        textTransform: 'uppercase',
+        color: theme.palette.text.secondary,
     }
 }))
+
+type LinkItem ={
+    name: string,
+    icon: JSX.Element | null,
+    url_link?: string,
+    disabled?: boolean,
+    subMenu?: Array< {
+        name: string,
+        icon: JSX.Element | null,
+        url_link?: string,
+        disabled?: boolean,
+    } >
+}
