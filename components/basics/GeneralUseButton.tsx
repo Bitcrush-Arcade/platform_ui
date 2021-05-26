1f3ee586f3fc40ce84f5fb80e17e3e7517550da9
jmanywhere <josmfernandez@gmail.com>
2021-05-26 07:34:17 -0600

    updated to match Material Standards

--- components/basics/GeneralUseButton.tsx
+++ components/basics/GeneralUseButton.tsx
@@ -1,47 +1,24 @@
-import { ReactNode, Ref } from 'react'
 // Material
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import Fab from '@material-ui/core/Fab'
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles'
+import Fab, {FabProps} from '@material-ui/core/Fab'
 
-const GeneralUseButton = (props: GeneralBtnProps ) => {
-
-  const { color = 'primary', onClick, href, target, children, ref } = props
-  const css = useStyles(props)
-
-  return <Fab 
-    variant="extended"
-    color={color}
-    className={css.walletBtn}
-    onClick={onClick}
-    component={ href ? 'a' : 'button'}
-    href={href}
-    target={target}
-    ref={ref}
-  >
-    {children}
-  </Fab>
-}
-
-type GeneralBtnProps ={
-  color?: "primary" | "secondary",
-  onClick?: (e) => void,
+type FabStyles ={
+  width?: string | number,
   href?: string,
   target?: string,
-  children?: ReactNode
-  ref?: Ref<HTMLAnchorElement>
-  width?: string | number
-}
+} & WithStyles< typeof styles > & FabProps
 
-export default GeneralUseButton
 
-const useStyles = makeStyles<Theme, GeneralBtnProps>( (theme:Theme) => createStyles({
-  walletBtn:{
-    width: props => props.width || null,
+const styles = (theme:Theme) => createStyles({
+  root:{
+    width: (props : FabStyles) => props.width || null,
     backgroundColor: 'transparent',
-    borderColor: props => theme.palette[props.color || 'primary'].main,
+    borderColor: (props : FabStyles) => theme.palette[props.color || 'primary'].main,
     borderWidth: 1,
     borderStyle: 'solid',
-    boxShadow: props => `inset 0 0 15px ${theme.palette[props.color || 'primary'].main}`,
+    boxShadow: (props : FabStyles) => `inset 0 0 15px ${theme.palette[props.color || 'primary'].main}`,
     color: theme.palette.grey[200],
   }
-}))
+})
+
+export default withStyles(styles)( (props: FabStyles) => <Fab {...props} variant="extended" component={ props.href ? 'a' : 'button' } target={props.target} />)
