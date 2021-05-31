e9a4aaa5d6a92bfde7ef76aa744736364ef463d3
jmanywhere <josmfernandez@gmail.com>
2021-05-31 12:58:02 -0600

    added token images, made token component

---
+++ components/tokens/Token2.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+
+const Token2 = (props: TokenImgProps ) => {
+  const { scale } = props
+  const width = 107 * scale
+  const height = 96 * scale
+  return <Image src="/token/Coins1.png" width={width} height={height}/>
+}
+
+type TokenImgProps ={
+  scale?: number
+}
+
+export default Token2
