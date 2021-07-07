4b3c4cb0f23cecba253c953799f7a4a3288a48a9
jmanywhere <josmfernandez@gmail.com>
2021-07-07 00:18:36 -0600

    connect to data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { useContext, useMemo, useState, useEffect, useCallback } from 'react'
+
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 // Material
 import { makeStyles, createStyles, Theme, useTheme } from "@material-ui/core/styles"
 import useMediaQuery from "@material-ui/core/useMediaQuery"
@@ -10,22 +13,64 @@ import Typography from '@material-ui/core/Typography'
 // Bitcrush Components
 import PageContainer from 'components/PageContainer'
 import Card from 'components/basics/Card'
+import Coin from 'components/tokens/Token2'
 import HarvestCard from 'components/pools/HarvestCard'
 // Icons
 import InvaderIcon, { invaderGradient } from 'components/svg/InvaderIcon'
 // utils
 import { currencyFormat } from 'utils/text/text'
+import { TransactionContext } from 'components/context/TransactionContext'
+import { useContract } from 'hooks/web3Hooks'
+import { getContracts } from 'data/contracts'
+import { useWeb3React } from '@web3-react/core'
+import BigNumber from 'bignumber.js'
 
 export default function Home() {
 
   const theme = useTheme()
   const isSm = useMediaQuery( theme.breakpoints.down('sm') )
+  const router = useRouter()
+  const { chainId, account } = useWeb3React()
+  const { tokenInfo, editTransactions } = useContext(TransactionContext)
+  const firstPool = useMemo( () => getContracts('singleAsset', chainId ), [chainId])
+  const { methods } = useContract(firstPool.abi, firstPool.address)
+
+  const [tvl, setTvl ] = useState<number>(0)
+  const [staked, setStaked ] = useState<number>(0)
 
   const [ gradient, gradientId ] = invaderGradient()
   const css = useStyles({ gradientId })
-  const totalValueLocked = 9526774.00154119
-  const maxWin = totalValueLocked * 0.01
-  const totalValueShared = 65010004.115487
+
+  useEffect( () => {
+
+    if(!methods) return
+    const getTvl = async () => {
+      const totalStaked = await methods.totalStaked().call()
+      const accountStake = await methods.stakings(account).call()
+      setTvl( new BigNumber(totalStaked).div( new BigNumber(10).pow(18) ).toNumber() )
+      setStaked( new BigNumber(accountStake?.stakedAmount || 0).div( new BigNumber(10).pow(18) ).toNumber() )
+    }
+    getTvl()
+  },[ methods, setTvl, account ])
+
+  const harvestAll = useCallback(()=>{
+    if(!methods) return
+    if( account && !staked)
+      return router.push("/mining","/mining",{ shallow: false})
+    methods.claim().send({ from: account })
+    .on('transactionHash', (tx) => {
+      console.log('hash', tx )
+      editTransactions(tx,'pending')
+    })
+    .on('receipt', ( rc) => {
+      console.log('receipt',rc)
+      editTransactions(rc.transactionHash,'complete')
+    })
+    .on('error', (error, receipt) => {
+      console.log('error', error, receipt)
+      receipt?.transactionHash && editTransactions( receipt.transactionHash, 'error', error )
+    })
+  },[methods, router, staked, account])
 
   return (<>
   <Head>
@@ -58,14 +103,19 @@ export default function Home() {
               <CardContent style={{paddingBottom: 16}}>
                 <Grid container justify="space-around">
                   <Grid item xs={12} md={'auto'}>
-                    <Typography variant="caption" component="div" align={isSm ? "center" : "left"} color="primary" style={{ textTransform: 'uppercase', opacity: 0.9 }}>
+                    <Typography variant="caption" component="div" 
+                      align={"center"
+                        //isSm ? "center" : "left"
+                      }
+                      color="primary" style={{ textTransform: 'uppercase', opacity: 0.9 }}
+                    >
                       Total Value Locked
                     </Typography>
-                    <Typography variant="h4" component="div" align={isSm ? "center" : "left"}>
-                      {currencyFormat(totalValueLocked,{ decimalsToShow: 0})}
+                    <Typography variant="h4" component="div" align={"center"}>
+                      {currencyFormat(tvl,{ decimalsToShow: 0})}
                     </Typography>
                   </Grid>
-                  <Divider orientation="vertical" flexItem/>
+                  {/* <Divider orientation="vertical" flexItem/> */}
                   <Grid item xs={12} md={'auto'}>
                     <Typography variant="caption" component="div" align="center" color="secondary" style={{ textTransform: 'uppercase', opacity: 0.9 }}>
                       Max Win
@@ -75,7 +125,7 @@ export default function Home() {
                       COMING SOON
                     </Typography>
                   </Grid>
-                  <Divider orientation="vertical" flexItem/>
+                  {/* <Divider orientation="vertical" flexItem/>
                   <Grid item xs={12} md={'auto'}>
                     <Typography variant="caption" component="div" align={isSm ? "center" : "right"} color="primary" style={{ textTransform: 'uppercase', opacity: 0.9 }}>
                       Total Value Shared
@@ -83,26 +133,29 @@ export default function Home() {
                     <Typography variant="h4" component="div" align={isSm ? "center" : "right"}>
                       {currencyFormat(totalValueShared,{ decimalsToShow: 0})}
                     </Typography>
-                  </Grid>
+                  </Grid> */}
                 </Grid>
               </CardContent>
             </Card>
             <Grid container justify="space-around" spacing={3} style={{marginTop: 32}}>
               <Grid item md={6}>
                 <HarvestCard title="Staking Pool" color="primary"
-                  stakedInfo={{
-                    title: "CRUSH to Harvest",
-                    amount: 547.2140070078,
-                    subtitle: "-$ 0.00",
-                    currency: "$",
-                  }}
                   rewardInfo={{
                     title: "CRUSH in Wallet",
-                    amount: 547.2140070078,
-                    subtitle: "-$ 0.00",
-                    currency: "$",
+                    amount: tokenInfo.weiBalance/(10 ** 18),
+                    subtitle: `$ ${currencyFormat(tokenInfo.crushUsdPrice * tokenInfo.weiBalance/(10 ** 18), { decimalsToShow: 2})}`,
                   }}
-                  action1Title="Harvest All"
+                  stakedInfo={{
+                    title: "CRUSH Staked",
+                    amount: staked,
+                    subtitle: `$ ${currencyFormat( staked * tokenInfo.crushUsdPrice, { decimalsToShow: 2 })}`,
+                  }}
+                  action1Title={ !account && 'Connect First' || staked > 0 && "Harvest All" || "Go to Pool"}
+                  icon={<Coin scale={0.75}/>}
+                  firstAction={harvestAll}
+                  btn1Props={{
+                    disabled: !account,
+                  }}
                 />
               </Grid>
               <Grid item md={6}>
