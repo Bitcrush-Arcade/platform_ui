fbcb6c4cae7908163beeff4ae7b69531cbeecd5e
jmanywhere <josmfernandez@gmail.com>
2021-07-09 13:37:01 -0600

    added lottery and referral pages

---
+++ pages/lottery.tsx
@@ -0,0 +1,20 @@
+// Next
+import Head from 'next/head'
+// Material
+import Typography from '@material-ui/core/Typography'
+// Bitcrush
+import PageContainer from 'components/PageContainer'
+
+const Trade = () => {
+  return <PageContainer>
+     <Head>
+      <title>BITCRUSH - Lottery (comingSoon)</title>
+      <meta name="description" content="Lottery Coming Soon"/>
+    </Head>
+    <Typography variant="h1" align="center">
+      LOTTERY COMING SOON
+    </Typography>
+  </PageContainer>
+}
+
+export default Trade
---
+++ pages/referral.tsx
@@ -0,0 +1,20 @@
+// Next
+import Head from 'next/head'
+// Material
+import Typography from '@material-ui/core/Typography'
+// Bitcrush
+import PageContainer from 'components/PageContainer'
+
+const Trade = () => {
+  return <PageContainer>
+    <Head>
+      <title>BITCRUSH - REFERRAL (comingSoon)</title>
+      <meta name="description" content="Referral Coming Soon"/>
+    </Head>
+    <Typography variant="h1" align="center">
+      REFERRAL COMING SOON
+    </Typography>
+  </PageContainer>
+}
+
+export default Trade
