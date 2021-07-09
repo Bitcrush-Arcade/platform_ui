9d2f279a4551f6df1250051757217aa4f18d4246
jmanywhere <josmfernandez@gmail.com>
2021-07-09 08:10:08 -0600

    update pool description

--- pages/mining.tsx
+++ pages/mining.tsx
@@ -24,9 +24,9 @@ const Mining = () => {
     <Grid item xs={10} sm={8} md={6}>
         <Descriptor
           title="Mining Pool"
-          description={`Stake CRUSH coins to earn APY as well as part of the House Edge.
-            Staking not only helps stabilize the CRUSH Economy, it also provides Bankroll for the games.
-            Due to the nature of gambling, this is riskier, but result in higher rewards than traditional staking.`}
+          description={`Stake CRUSH coins in our single asset staking pool to earn APY that is Auto Compounded.
+            You can help the community by calling the auto Compound function and earning a bounty % of the entire pending reward amount. 
+            All you need to do it press the Claim button,  and watch the rewards compound for everyone staked in the pool.`}
         />
       </Grid>
       <Grid item>
