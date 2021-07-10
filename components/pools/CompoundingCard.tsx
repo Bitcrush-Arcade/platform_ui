37f602b593310206925010a4cd175d05c336cb97
jmanywhere <josmfernandez@gmail.com>
2021-07-10 10:57:45 -0600

    fix typo, added claim modal

--- components/pools/CompoundingCard.tsx
+++ components/pools/CompoundingCard.tsx
@@ -1,6 +1,10 @@
 import { useState, useEffect, useContext, useCallback, useMemo } from "react"
 // Material
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles"
+import Checkbox from '@material-ui/core/Checkbox'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import CardContent from "@material-ui/core/CardContent"
@@ -30,6 +34,9 @@ const CompoundingCard = (props: CompoundingCardProps ) => {
   const { methods } = useContract( stakeContract.abi, stakeContract.address )
   const [rewardToDistribute, setRewardToDistribute ] = useState<BigNumber>( new BigNumber(0) )
   const [hydrate, setHydrate] = useState<boolean>(false)
+
+  const [ showWarning, setShowWarning ] = useState<boolean>(false)
+  const [ agreed, setAgreed ] = useState<boolean>(false)
   
   const toggleHydrate = useCallback(() => setHydrate(p => !p) ,[setHydrate])
   
@@ -58,6 +65,11 @@ const CompoundingCard = (props: CompoundingCardProps ) => {
   }, [rewardToDistribute, tokenInfo])
 
   const claim = () => {
+    if(!showWarning){
+      setShowWarning(true)
+      return
+    }
+    setShowWarning(false)
     methods.compoundAll().send({ from: account })
       .on('transactionHash', tx => editTransactions(tx, 'pending', { description: "Execute Auto Compound" }))
       .on('receipt', rct =>{
@@ -70,7 +82,10 @@ const CompoundingCard = (props: CompoundingCardProps ) => {
       } )
   }
 
-  return <Card background="light" shadow="dark" className={ css.claimCard } >
+  const exitClaim = () => setShowWarning( false )
+
+  return <>
+  <Card background="light" shadow="dark" className={ css.claimCard } >
     <CardContent className={ css.cardContent }>
       <Grid container justify="space-between" alignItems="center">
         <Grid item>
@@ -83,7 +98,7 @@ const CompoundingCard = (props: CompoundingCardProps ) => {
             arrow
             title={
               <Typography variant="body2" style={{ whiteSpace: 'pre-line', margin: 8}}>
-                This bounty is given as a reward for prividing a service to other users.{'\n\n'}
+                This bounty is given as a reward for providing a service to other users.{'\n\n'}
                 Whenever you successfully claim the bounty, you&apos;re also helping out by activating the Auto CRUSH Pool&apos;s compounding function for everyone.{'\n\n'}
                 <strong>
                   Auto-compound Bounty of 0.1% of all Auto CRUSH pool users pending yield.
@@ -108,13 +123,37 @@ const CompoundingCard = (props: CompoundingCardProps ) => {
           </Typography>
         </Grid>
         <Grid item>
-          <Button size="small" width={80} color="primary" onClick={claim} disabled={!methods}>
+          <Button size="small" width={80} color="primary" onClick={claim} disabled={!methods || (showWarning && !agreed )}>
             Claim
           </Button>
         </Grid>
       </Grid>
     </CardContent>
   </Card>
+  <Dialog open={showWarning} onClose={exitClaim} PaperComponent={ paperProps => <Card {...paperProps} style={{paddingBottom: 16}}/>}>
+    <DialogContent>
+      <Typography paragraph style={{whiteSpace: 'pre-line' }} align="justify">
+        Due to excessive gas fees charged by the claim function, please only use when claim amount is Higher than gas fee. Otherwise you will be losing funds.
+        {'\n'}If you don’t understand, please have the mods explain this to you.
+      </Typography>
+      <Typography align="center" paragraph>
+        Are you sure?
+      </Typography>
+      <Grid container justify="center" spacing={2}>
+        <Grid item>
+          <Button color="secondary" width={120} onClick={claim}>
+            CLAIM
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button color="primary" width={120} onClick={exitClaim}>
+            EXIT
+          </Button>
+        </Grid>
+      </Grid>
+    </DialogContent>
+  </Dialog>
+  </>
 }
 
 export default CompoundingCard
