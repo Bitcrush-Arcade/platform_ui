f250d5011152a17f2d57b20954f94fa70e554a5b
jmanywhere <josmfernandez@gmail.com>
2021-09-09 23:38:56 -0600

    EP good for testnet

--- pages/api/withdrawForUser.ts
+++ pages/api/withdrawForUser.ts
@@ -11,21 +11,29 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
   //   return res.status(400).send({ message: 'Invalid Request'})
 
   // SETUP BLOCKCHAIN
-  const provider = 'https://data-seed-prebsc-2-s2.binance.org:8545/'
+  const provider = 'https://data-seed-prebsc-2-s3.binance.org:8545/'
   const web3 = new Web3( new Web3.providers.HttpProvider( provider ) )
   const setup = getContracts('liveWallet', 97 )
   const contract = await new web3.eth.Contract( setup.abi, setup.address )
   // START BALANCE
   const ogBalance = await contract.methods.betAmounts(account).call()
   // Authorize Owner
-  // const ownerAccount = await web3.eth.accounts.privateKeyToAccount( process.env.OWNER_PKEY)
-  // await contract.methods.withdrawBetForUser(amount, account).send({ from: ownerAccount.address })
+  const ownerAccount = await web3.eth.accounts.privateKeyToAccount( process.env.OWNER_PKEY)
+  const txData = await contract.methods.withdrawBetForUser(amount, account).encodeABI()
+  const signedTx = await ownerAccount.signTransaction({
+    to: setup.address,
+    data: txData,
+    gas: 20000000,
+  })
+  await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+    .on('receipt', r => console.log('onReceipt', r))
+    .catch( e => console.log('error',e))
+
   // NEW BALANCE
-  // const newBalance = await contract.methods.balanceOf(account).call()
+  const newBalance = await contract.methods.balanceOf(account).call()
   res.send({ 
-    // account: ownerAccount.address,
-    methods: contract,
-    initBalance: ogBalance,
-    // updatedBalance: newBalance
+    account: ownerAccount.address,
+    initBalance: ogBalance?.balance,
+    updatedBalance: newBalance
   })
 }
