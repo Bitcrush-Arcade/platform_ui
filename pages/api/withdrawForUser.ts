36a55c6380d12435bf7edf8c1afce4589e00b1c6
jmanywhere <josmfernandez@gmail.com>
2021-09-14 01:23:04 -0600

    fetched from API... needs testing

--- pages/api/withdrawForUser.ts
+++ pages/api/withdrawForUser.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import Web3 from 'web3'
+// 3rd Party
+import differenceInSeconds from "date-fns/differenceInSeconds"
+import BigNumber from "bignumber.js"
 // CONTRACT
 import { getContracts } from 'data/contracts'
 
@@ -13,10 +16,22 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
   // SETUP BLOCKCHAIN
   const provider = 'https://data-seed-prebsc-2-s3.binance.org:8545/'
   const web3 = new Web3( new Web3.providers.HttpProvider( provider ) )
-  const setup = getContracts('liveWallet', 97 )
+  const setup = getContracts('liveWallet', chain )
   const contract = await new web3.eth.Contract( setup.abi, setup.address )
   // START BALANCE
   const ogBalance = await contract.methods.betAmounts(account).call()
+  const lockDuration = await contract.methods.lockPeriod().call()
+  const serverBalance = await fetch(`http://104.219.251.99:5019/users/wallet/lw/${account}`)
+    .then( r => r.json() )
+    .then( data => data.user_balance )
+    .catch( e => console.log( 'error', e))
+  // CHECK WHICH AMOUNT IS MAX AMOUNT
+  const timelockActive =  differenceInSeconds( new Date() , new Date( new BigNumber(ogBalance.lockTimeStamp).plus(lockDuration).times(1000).toNumber() )) > 0
+  const maxWithdrawAmount =  timelockActive ? serverBalance : ogBalance 
+
+  if( amount > maxWithdrawAmount)
+    return res.status(400).send({ message: "Can't withdraw more than max amount available"})
+
   // Authorize Owner
   const ownerAccount = await web3.eth.accounts.privateKeyToAccount( process.env.OWNER_PKEY)
   const txData = await contract.methods.withdrawBetForUser(amount, account).encodeABI()
