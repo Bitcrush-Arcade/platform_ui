29229cc94bd8849d59aa4fea59772434af77c1f1
jmanywhere <josmfernandez@gmail.com>
2021-09-24 00:25:22 -0600

    game API servers

--- utils/servers.ts
+++ utils/servers.ts
@@ -4,3 +4,8 @@ export const servers = {
   'development': "http://104.219.251.99:5019",
   "production": "http://104.219.251.99:5019" // TODO , GET PRODUCTION READY SERVER URL
 }
+
+export const gameApi = {
+  'development': "http://104.219.251.99:3000",
+  "production": "http://104.219.251.99:3000" // TODO , GET PRODUCTION READY SERVER URL
+}
