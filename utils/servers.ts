3e3a91f7ff4030ef35ae5640d6ac1004f5d93943
jmanywhere <josmfernandez@gmail.com>
2021-09-24 16:10:14 -0600

    added dragon servers

--- utils/servers.ts
+++ utils/servers.ts
@@ -9,3 +9,8 @@ export const gameApi = {
   'development': "http://104.219.251.99:3000",
   "production": "http://104.219.251.99:3000" // TODO , GET PRODUCTION READY SERVER URL
 }
+
+export const dragonEp = {
+  'development': 'https://staging-api.dragongaming.com',
+  "production": 'https://staging-api.dragongaming.com' // TODO , GET PRODUCTION READY SERVER URL
+}
