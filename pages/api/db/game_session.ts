e600bd45ed6374402ea1a46de1d3b27d94e3b13b
jmanywhere <josmfernandez@gmail.com>
2021-09-24 00:26:15 -0600

    safe http api layer- WIP

---
+++ pages/api/db/game_session.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { toWei } from 'web3-utils'
+import { gameApi } from 'utils/servers'
+
+export default async function getGameSession(req : NextApiRequest, res: NextApiResponse){
+  
+  const { wallet, country } = JSON.parse(req.body)
+
+  if(req.method !== 'POST' || !wallet){
+    res.status(400).json({ message: 'Invalid Request'})
+    return
+  }
+
+  await fetch(`${gameApi[ process.env.NODE_ENV ]}/api/games/generate_session`,{
+    method: 'POST',
+    headers:{
+      origin: 'http://localhost:3000'
+    },
+    body: JSON.stringify({
+      wallet_address: wallet,
+      country: country
+    })
+  })
+    .then( d => d.json() )
+    .then( data => {
+      if( data?.statusCode > 200 )
+        res.status(503).json({ message: 'Database is not responding'})
+      else
+        res.status(200).json(data)
+    } )
+
+}
