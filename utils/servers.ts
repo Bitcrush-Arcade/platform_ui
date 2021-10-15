313f134f5e7806806a49972c36eabad1ee8fcb1b
jmanywhere <josmfernandez@gmail.com>
2021-10-15 09:05:56 -0600

    fix server URL to use

--- utils/servers.ts
+++ utils/servers.ts
@@ -1,13 +1,13 @@
 
 
 export const servers = {
-  'development': "http://104.219.251.99:5000",
-  "production": "http://104.219.251.99:5000" // TODO , GET PRODUCTION READY SERVER URL
+  'development': "https://diceinvaders.bitcrush.com",
+  "production": "https://diceinvaders.bitcrush.com" // TODO , GET PRODUCTION READY SERVER URL
 }
 
 export const gameApi = {
-  'development': "http://104.219.251.99:3000",
-  "production": "http://104.219.251.99:3000" // TODO , GET PRODUCTION READY SERVER URL
+  'development': "https://diceinvaders.bitcrush.com",
+  "production": "https://diceinvaders.bitcrush.com" // TODO , GET PRODUCTION READY SERVER URL
 }
 
 export const dragonEp = {
