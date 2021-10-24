0e10671e0074b39bb4024b2b53ca7e6e0d6e488f
jmanywhere <josmfernandez@gmail.com>
2021-10-24 10:04:23 -0600

    updated blacklist text

---
+++ data/texts.tsx
@@ -0,0 +1,35 @@
+import Link from '@material-ui/core/Link'
+export const blacklistExplanation = <>{`
+  Bitcrush.com is committed to support responsible gaming, so we added a Self Blacklist function to our contract that allows you to blacklist yourself from making deposits to the Live Wallet for gameplay. 
+
+  Please note: BLACKLISTING WILL AFFECT LIVE WALLET DEPOSITS ONLY. 
+
+  This means that any funds you have deposited can be claimed by you at any time. So if you feel in distress, you may blacklist yourself immediately and then claim your unused funds at any time. 
+
+  Gambling should be for entertainment purposes and Bitcrush.com is focused on offering you the best gaming experience. However a small percentage of the population can have adverse effects to gambling. 
+
+  Test to see if you have a gambling problem
+  It's important to know if gambling is causing you or others around you harm. Please have a look at the following questions:
+
+  - Is it difficult to stop playing?
+  - Do you neglect work, studies, family, friends or personal needs because of      gambling?
+  - Do you ever gamble more than you can afford to lose?
+  - Does gambling make you feel irritable or depressed?
+
+  If you have answered yes to any of these questions, you may have a gambling problem. We recommend looking at the following sites that can provide help and advice:
+
+  Self Exclusion-
+
+  Call the Self Blacklist function to stop yourself from being able to deposit funds into the Live Wallet, then withdraw your funds from the Live Wallet. 
+
+  Seek Help-
+
+  Here are a list of resources as you navigate the waters of Responsible Gaming:
+  • `}<Link color="secondary" href="http://www.gamblersanonymous.org/ga/" rel="nonreferrer noopener">Gamblers Anonymous</Link>{`, probably the best-known organization for name recognition alone. This is a great place to receive support from other anonymous folks dealing with gambling addiction. Locate a meeting near you or talk to someone online with one click.
+  • `}<Link color="secondary" href="https://www.ncpgambling.org/help-treatment/national-helpline-1-800-522-4700/" rel="nonreferrer noopener">National Council on Problem Gambling</Link>{`, which includes a toll-free helpline that’s available 24/7, is completely anonymous, and total confidential. If you’re unsure whether you have a problem, call them right now at 1-800-522-4700.
+  • `}<Link color="secondary" href="https://www.icrg.org/" rel="nonreferrer noopener">International Center for Responsible Gaming</Link>{`, which helps folks around the world who are dealing with online gambling addiction. This is an especially useful resource for those who resort to illegal, offshore gambling sites to circumvent local laws.
+
+
+  Questions?
+  For any other questions regarding responsible gaming please contact our support.
+`}</>
