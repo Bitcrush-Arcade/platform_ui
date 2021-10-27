636fc2fe0fd3d171be78d35e7a00eef66b0d5aab
jmanywhere <josmfernandez@gmail.com>
2021-10-27 13:41:23 -0600

    WIP not implemented

---
+++ pages/api/contracts/distributionCalculator.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+// Web3
+import Web3 from 'web3'
+import BigNumber from 'bignumber.js'
+// Data
+import { getContracts } from 'data/contracts'
+
+const calculateDistribution = async(req: NextApiRequest, res: NextApiResponse)=>{}
+
+export default calculateDistribution
