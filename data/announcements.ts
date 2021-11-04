f8c02cbd1612d15ba61a363a9b41fbe735cef849
jmanywhere <josmfernandez@gmail.com>
2021-11-03 20:00:23 -0600

    tweak announcements, added more announcements

--- data/announcements.ts
+++ data/announcements.ts
@@ -2,7 +2,7 @@ const highlightedAnnouncements : Array<Announcement> = [
   {
     name: 'Live Now',
     img: '/assets/announcements/1-live-now-banner.png',
-    imgMobile: '',
+    imgMobile: '/assets/announcements/1-live-now-mobile.png',
     link: '/games/diceInvaders',
     target: null,
     rel: null,
@@ -10,11 +10,19 @@ const highlightedAnnouncements : Array<Announcement> = [
   {
     name: 'Staking 2.0',
     img: '/assets/announcements/2-stake-v2-banner.png',
-    imgMobile: '',
-    link: '',
+    imgMobile: '/assets/announcements/2-stake-v2-mobile.png',
+    link: '/mining',
     target: null,
     rel: null,
-  }
+  },
+  {
+    name: 'Pearzap Partnership',
+    img: '/assets/announcements/3-pearzap-banner.png',
+    imgMobile: '/assets/announcements/3-pearzap-mobile.png',
+    link: 'https://bsc.pearzap.com/the-garden',
+    target: "_blank",
+    rel: "noopener nonreferrer",
+  },
 ]
 
 export const height= 310
