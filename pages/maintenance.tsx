440f6ef42e7a67c9f35de9f9a8eb99d8fd3557ea
jmanywhere <josmfernandez@gmail.com>
2021-11-10 08:55:03 -0600

    added maintenance page and redirect

---
+++ pages/maintenance.tsx
@@ -0,0 +1,20 @@
+// Next
+import Head from 'next/head'
+// Material
+import Typography from '@material-ui/core/Typography'
+// Bitcrush
+import PageContainer from 'components/PageContainer'
+
+const Maintenance = () => {
+  return <PageContainer>
+    <Head>
+      <title>BITCRUSH - UNDER MAINTENANCE</title>
+      <meta name="description" content="Trading Coming Soon"/>
+    </Head>
+    <Typography variant="h1" align="center">
+      Under Maintenance, we apologize for any inconveniences.
+    </Typography>
+  </PageContainer>
+}
+
+export default Maintenance
