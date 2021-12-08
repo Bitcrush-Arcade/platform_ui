448f75f1572dcf05d4bf294f722b76334244bc9f
jmanywhere <josmfernandez@gmail.com>
2021-12-07 20:40:18 -0600

    add logger to withdraw for user

--- pages/api/withdrawForUser.ts
+++ pages/api/withdrawForUser.ts
@@ -7,6 +7,7 @@ import BigNumber from "bignumber.js"
 import { getContracts } from 'data/contracts'
 import { servers } from 'utils/servers'
 import { toWei } from 'web3-utils'
+import logger from 'utils/logger'
 
 BigNumber.config({ DECIMAL_PLACES: 18 })
 
@@ -14,6 +15,7 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
 
   const bodyData: { amount?: number, account?: string, chain?: number } = JSON.parse(req.body || '{}')
   const { amount, account, chain } = bodyData
+  const logdata:any = {...bodyData}
   chain == 56 && console.log('Requester',{ amount, account })
   if(!amount || !account || !chain)
     return res.status(400).send({ message: 'Invalid Request'})
@@ -26,7 +28,9 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
     body: JSON.stringify({ account: account })
   }).then( r => r.json() )
   console.log('lock status', lock.lockWithdraw)
+  logdata.lock = lock.lockWithdraw
   if(lock.lockWithdraw){
+    logger.info(logdata)
     res.status(200).json({ message: 'Withdraw locked for 90 secs, please try later', timelock: lock.lockWithdraw })
     return
   }
@@ -47,6 +51,7 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
   .then( r => r.json() )
   .then( data =>  parseInt(toWei(`${new BigNumber(data.balance).toFixed(18,1)}`)) )
     .catch( e => {
+      logger.error({error: e, logdata}, 'Server Balance is not available')
       res.status(400).json({ message: 'Server Balance is not available', error: e})
       return 'Error'
     })
@@ -54,9 +59,11 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
   // CHECK WHICH AMOUNT IS MAX AMOUNT
   const timelockActive =  differenceInSeconds( new Date() , new Date( new BigNumber(ogBalance.lockTimeStamp).plus(lockDuration).times(1000).toNumber() )) > 0
   const maxWithdrawAmount =  timelockActive ? serverBalance : ogBalance.balance 
-  console.log( 'balances', { serverBalance, ogBalance: new BigNumber(ogBalance.balance).div(10**18).toFixed(18,1) })
-  if( new BigNumber( amount ).isGreaterThan( new BigNumber(maxWithdrawAmount) ) )
+  logdata.amounts = { serverBalance, ogBalance: new BigNumber(ogBalance.balance).div(10**18).toFixed(18,1) }
+  if( new BigNumber( amount ).isGreaterThan( new BigNumber(maxWithdrawAmount) ) ){
+    logger.error(logdata, 'Server Balance is not available')
     return res.status(400).send({ error: "Can't withdraw more than max amount available"})
+  }
 
   // Authorize Owner
   const ownerAccount = await web3.eth.accounts.privateKeyToAccount( process.env.OWNER_PKEY)
@@ -71,6 +78,7 @@ export default async function withdrawForUser( req: NextApiRequest, res: NextApi
 
     return web3.eth.sendSignedTransaction(signedTx.rawTransaction)
     .on( 'transactionHash', tx => {
+      logger.info({logdata, tx},"success in creating hash")
       res.status(200).send({ txHash: tx})
       hashSent = true
     })
