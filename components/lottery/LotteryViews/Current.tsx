99db92caf9782fdbe103675354bbb7c004d30dc6
jmanywhere <josmfernandez@gmail.com>
2021-12-22 02:08:27 -0600

    fix key lint issue

--- components/lottery/LotteryViews/Current.tsx
+++ components/lottery/LotteryViews/Current.tsx
@@ -34,8 +34,8 @@ export default function BasicTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow>
+          {rows.map((row, rowIndex) => (
+            <TableRow key={`current-view-${rowIndex}`}>
               <TableCell>{row.roundInfo}</TableCell>
               <TableCell>{row.teamInfo}</TableCell>
             </TableRow>
