143947bf25ca403242a5c0ce8fb23108e6136283
jmanywhere <josmfernandez@gmail.com>
2021-12-22 15:53:39 -0600

    added invader icons

---
+++ components/svg/invaders/Invader1.tsx
@@ -0,0 +1,7 @@
+import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
+
+const Invader1 = ( props: SvgIconProps ) => <SvgIcon {...props} viewBox="0 0 24 24" id="Invader_1" data-name="Invader 1" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M7.2 0H4.8V2.4H7.2V4.8H4.8V7.2H2.4V9.6H0V12H2.4V14.4H4.8V16.8H7.2V19.2H4.8V21.6V24H7.2V21.6V19.2H9.6V21.6H12H14.4V19.2H16.8V21.6V24H19.2V21.6V19.2H16.8V16.8H19.2V14.4H21.6V12H24V9.6H21.6V7.2H19.2V4.8H16.8V2.4H19.2V0H16.8V2.4H14.4V4.8H9.6V2.4H7.2V0ZM16.8 12V9.6H14.4V12H16.8ZM7.2 12H9.6V9.6H7.2V12Z"/>
+</SvgIcon>
+
+export default Invader1
