b4f51def6b3b80385a6657e05c3b26d1b7442ea9
yawgmoth19 <urzasguilt4@gmail.com>
2021-12-21 18:24:04 -0600

    Placeholders

--- components/lottery/LotteryViews/Current.tsx
+++ components/lottery/LotteryViews/Current.tsx
@@ -1,4 +1,47 @@
-const Current = () => {
-return null
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  roundInfo: string,
+  teamInfo: any,
+  
+) {
+  return { roundInfo, teamInfo };
 }
-export default Current
+
+const rows = [
+  createData('WINNING TEAM', 'PLACEHOLDER'),
+  createData('TICKET INFO', 'PLACEHOLDER'),
+  
+];
+{/*const teamArray = Array()
+const ticketArray = Array()*/}
+
+export default function BasicTable() {
+  return (
+    
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ROUND 3 </TableCell>
+            <TableCell>DATE AND TIME OF ROUND</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow>
+              <TableCell>{row.roundInfo}</TableCell>
+              <TableCell>{row.teamInfo}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    
+  );
+}
