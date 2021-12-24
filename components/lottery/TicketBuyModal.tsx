d0849728f322b08294f1b6d1fbe153212e7c4747
jmanywhere <josmfernandez@gmail.com>
2021-12-23 18:35:52 -0600

    rework form in formik, started edit and thanks

--- components/lottery/TicketBuyModal.tsx
+++ components/lottery/TicketBuyModal.tsx
@@ -2,19 +2,23 @@ import { useEffect, useState } from 'react'
 import { useImmer } from 'use-immer'
 import BigNumber from 'bignumber.js'
 import random from 'lodash/random'
+import { Formik, Field, Form } from 'formik'
+import { TextField } from 'formik-mui'
 // Material
+import type { Theme } from '@mui/material/styles'
 import { alpha } from '@mui/material/styles'
+import MButton from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import IconButton from '@mui/material/IconButton'
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import Tooltip from '@mui/material/Tooltip'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import { useWeb3React } from '@web3-react/core'
 // Icons
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import CloseIcon from '@mui/icons-material/Close'
+import EditIcon from '@mui/icons-material/Edit'
 // Bitcrush UI
 import Button from 'components/basics/GeneralUseButton'
 import Currency from 'components/basics/Currency'
@@ -28,6 +32,7 @@ import { getContracts } from 'data/contracts'
 import { currencyFormat } from 'utils/text/text'
 import { standardizeNumber, getTicketDigits } from 'utils/lottery'
 import NumberInvader from 'components/lottery/NumberInvader'
+import useCoin from 'hooks/useCoin'
 
 type TicketBuyModalProps ={
   open: boolean;
@@ -37,7 +42,9 @@ type TicketBuyModalProps ={
 type LotteryInfo={
   currentRound: number,
   ticketPrice: BigNumber,
+  userBalance: BigNumber,
 }
+
 const TicketBuyModal = (props: TicketBuyModalProps) => {
 
   const { open, onClose } = props;
@@ -48,129 +55,69 @@ const TicketBuyModal = (props: TicketBuyModalProps) => {
   // 3 -> Thanks page
   const [ step, setStep ] = useState<number>(0)
   // Lottery Info
-  const [ lotteryInfo, setLotteryInfo ] = useImmer<LotteryInfo|null>(null)
+  const [ lotteryInfo, setLotteryInfo ] = useImmer<LotteryInfo>({ currentRound: 0, ticketPrice: new BigNumber(0), userBalance: new BigNumber(0) })
   // Ticket Info
   const [ tickets, setTickets ] = useImmer<Array<string>>([])
+  // Ticket Editor
+  const [ selectedTicket, setSelectedTicket ] = useState<number>(0)
   // BlockChain
   const { account, chainId } = useWeb3React()
-  const { tokenInfo } = useTransactionContext()
+  const { approve, isApproved, coinMethods, getApproved} = useCoin()
   const lotteryContract = getContracts('lottery', chainId)
   const { methods: lotteryMethods } = useContract( lotteryContract.abi, lotteryContract.address )
 
+  // Get Lottery Info
   useEffect( () => {
     if(!lotteryMethods) return
     const getLotteryData = async () => {
       const currentRound = new BigNumber(await lotteryMethods.currentRound().call()).toNumber();
       const ticketPrice = new BigNumber(await lotteryMethods.ticketValue().call());
-      setLotteryInfo({
-        currentRound,
-        ticketPrice
+      setLotteryInfo( draft => {
+        draft.currentRound = currentRound
+        draft.ticketPrice = ticketPrice
       })
     }
     getLotteryData()
   },[lotteryMethods, setLotteryInfo])
+  // Get Lottery Approval
+  useEffect(() => {
+    if(!account || isApproved) return
+    getApproved(lotteryContract.address)
+  },[coinMethods, getApproved, lotteryContract.address, account, isApproved])
 
-  const balanceError = lotteryInfo?.ticketPrice.times(tickets.length).isGreaterThan(tokenInfo.weiBalance)
-  const maxBuyError = (tickets.length) > 100
-
-  const hasError = balanceError || maxBuyError
+  useEffect( () => {
+    if(step !== 0 || !account || !coinMethods) return
+    const getData = async () => {
+      const userBalance = new BigNumber( await coinMethods.balanceOf(account).call().catch( (e: any) => 0 ) )
+      setLotteryInfo( draft => {
+        draft.userBalance = userBalance
+      })
+    }
+    getData()
+  },[step, coinMethods, account, setLotteryInfo])
 
   const closeModal = () => {
     onClose()
     setTickets([])
     setStep(0)
   }
-
-  const BuyStep = () => <>
-    <Stack direction="row" justifyContent="space-between" px={3} mb={2}>
-      <Typography color="textSecondary">
-        Ticket Cost
-      </Typography>
-      <Typography color="primary">
-        {currencyFormat( lotteryInfo?.ticketPrice.toString() || 0,{ isWei:true, decimalsToShow: 0 })}
-        &nbsp;CRUSH
-      </Typography>
-    </Stack>
-    <Stack direction="row" justifyContent="space-between" px={3}>
-      <Typography variant="subtitle2" color="textSecondary">
-        Round:
-      </Typography>
-      <Typography variant="subtitle2" fontWeight={600}>
-        {lotteryInfo?.currentRound}
-      </Typography>
-    </Stack>
-    <TextField
-      value={tickets.length}
-      color="primary"
-      onChange={ e => {
-        const valueNumber = parseInt( e.target.value )
-        if(isNaN(valueNumber))
-          return
-        setTickets( new Array(Math.abs(valueNumber)).fill(0).map( v => standardizeNumber(random(0,999999, false)).toString() ) )
-      }}
-      inputProps={{
-        inputMode: 'numeric',
-        pattern: '[0-9]*'
-      }}
-      InputProps={{
-        endAdornment:<Typography variant="subtitle2" color="textSecondary" sx={{pl:2}}>
-          {currencyFormat(tokenInfo.weiBalance.toString(), { isWei: true, decimalsToShow: 4})}
-          &nbsp;CRUSH
-        </Typography>,
-        onFocus: (e: React.FocusEvent<HTMLInputElement>) => e.target.select(),
-        sx: theme => ({
-          borderRadius: 32,
-          pl: 1,
-          backgroundColor: theme.palette.mode ==='dark' ? 'rgba(0,0,0,0.4)' : alpha(theme.palette.primary.light,0.4)
-        })
-      }}
-      error={hasError}
-      helperText={
-        <Typography color="error" variant="caption" sx={{pt: 1, pb:1}} component="div">
-          {balanceError ? 'Insufficient Balance ' :''}
-          {maxBuyError ? "Can't buy more than 100 per transaction " : ''}
-          &nbsp;
-        </Typography>
-      }
-    />
-    <Stack direction="row" justifyContent="space-between" px={3} mb={2}>
-      <Typography>
-        Total
-      </Typography>
-      <Typography color="primary">
-        <Currency value={ lotteryInfo?.ticketPrice.times(tickets.length) || 0} decimals={0} isWei/>
-        &nbsp;CRUSH
-      </Typography>
-    </Stack>
-    <Stack spacing={2}>
-      <Button color="primary" background="primary" onClick={()=>setStep(1)}
-        disabled={hasError || tickets.length == 0}
-      >
-        Choose Tickets
-      </Button>
-      <Button color="secondary"
-        disabled={hasError || tickets.length == 0}
-      >
-        Lucky Draw
-      </Button>
-      <Typography variant="subtitle2" px={2} whiteSpace="pre-line">
-        &quot;Choose Tickets&quot; Shows you our draw selections and allows you to edit your invader team to customize your perfect team.{'\n'}
-        &quot;Lucky Draw&quot; chooses your ticket numbers randomly with no duplicates on this draw.{'\n'}
-        Purchases are final.
-      </Typography>
-    </Stack>
-  </>
-
   return <Dialog 
     open={open}
     PaperComponent={FormComponent}
-    PaperProps={{sx: {pt: 2}}}
+    PaperProps={{sx: {pt: 2, maxWidth: '360px'}}}
   >
     <Stack direction="row" justifyContent="space-between">
       <div>
         { step > 0 && 
           <Tooltip arrow title={<Typography variant="subtitle1">Back</Typography>}>
-            <IconButton size="small" onClick={()=>setStep( p => p - 1 )}>
+            <IconButton size="small" 
+              onClick={()=>setStep( p => {
+                if(step < 3) 
+                  return p - 1
+                else
+                  return 1
+              } )}
+            >
               <ArrowBackIcon fontSize="inherit"/>
             </IconButton>
           </Tooltip>
@@ -184,37 +131,178 @@ const TicketBuyModal = (props: TicketBuyModalProps) => {
     </Stack>
     <Typography variant="h5" align="center" fontWeight={600} sx={{ textTransform: 'uppercase', pb:3}}>
       { step === 0 && "Buy Tickets"}
-      { step === 1 && `ROUND ${lotteryInfo?.currentRound}`}
+      { step === 1 && `ROUND ${lotteryInfo.currentRound}`}
+      { step === 2 && `Edit Ticket ${selectedTicket + 1}`}
+      { step === 3 && `Thanks for participating`}
     </Typography>
-    { step === 0 && <BuyStep/>}
-    {
-      step === 1 && <>
-      <Typography variant="subtitle1" align="center" fontWeight={600}>
-        Your Tickets
-      </Typography>
-      {tickets.map( (ticketNumber, ticketIndex) => {
-        const ticketDigits = ticketNumber.split('')
-        console.log(ticketDigits)
-        return <Stack key={`tickets-to-buy-${ticketIndex}`}>
-          <Typography variant="subtitle2" color="textSecondary">
-            #{ticketIndex+1}
-          </Typography>
-          <Paper
-            sx={ theme => ({
-              backgroundColor: theme.palette.mode == "dark" ? "#0C0E22" : theme.palette.primary.dark,
-              borderRadius: 3,
-              px: 2,
-              py: 2,
-            })}
+    { 
+      step === 0 &&
+        <>
+          <Stack direction="row" justifyContent="space-between" px={3} mb={2}>
+            <Typography color="textSecondary">
+              Ticket Cost
+            </Typography>
+            <Typography color="primary">
+              {currencyFormat( lotteryInfo.ticketPrice.toString() || 0,{ isWei:true, decimalsToShow: 0 })}
+              &nbsp;CRUSH
+            </Typography>
+          </Stack>
+          <Stack direction="row" justifyContent="space-between" px={3}>
+            <Typography variant="subtitle2" color="textSecondary">
+              Round:
+            </Typography>
+            <Typography variant="subtitle2" fontWeight={600}>
+              {lotteryInfo.currentRound}
+            </Typography>
+          </Stack>
+          <Formik
+            initialValues={{ ticketAmount: '', instant: false }}
+            onSubmit={(values) => {
+              console.log(values)
+              const ticketArray = new Array( parseInt(values.ticketAmount)).fill('0000000')
+              if(values.instant)
+                setTickets( ticketArray.map( x => standardizeNumber( random(0,999999) ).toString() ))
+              else
+                setTickets( ticketArray)
+              setStep(1)
+            }}
           >
-            <Stack direction="row">
-              <NumberInvader twoDigits={[ticketDigits[1], ticketDigits[2]]}/>
-              <NumberInvader twoDigits={[ticketDigits[3], ticketDigits[4]]}/>
-              <NumberInvader twoDigits={[ticketDigits[5], ticketDigits[6]]}/>
+            { ({ values, setFieldValue, submitForm }) => {
+              const balanceError = lotteryInfo.ticketPrice.times(values.ticketAmount).isGreaterThan(lotteryInfo.userBalance)
+              const maxBuyError = parseInt(values.ticketAmount) > 100
+              const hasError = balanceError || maxBuyError
+              return <Form>
+                <Field name="ticketAmount"
+                  component={TextField}
+                  color="primary"
+                  inputProps={{
+                    inputMode: 'numeric',
+                    pattern: '[0-9]*'
+                  }}
+                  fullWidth
+                  InputProps={{
+                    endAdornment: 
+                      <MButton onClick={()=>setFieldValue('ticketAmount',100)} color="secondary" sx={{fontWeight: 600}}>
+                        Max
+                      </MButton>,
+                    onFocus: (e: React.FocusEvent<HTMLInputElement>) => e.target.select(),
+                    sx: (theme:Theme ) => ({
+                      borderRadius: 32,
+                      pl: 1,
+                      backgroundColor: theme.palette.mode ==='dark' ? 'rgba(0,0,0,0.4)' : alpha(theme.palette.primary.light,0.4)
+                    })
+                  }}
+                  error={hasError}
+                  helperText={hasError ? <>
+                      {balanceError ? 'Insufficient Balance ' :''}
+                      {maxBuyError ? "Can't buy more than 100 per transaction " : ''}
+                  </> : " "}
+                />
+                <Stack direction="row" justifyContent="space-between" px={3}>
+                  <Typography>
+                    Balance
+                  </Typography>
+                  <Typography color="primary">
+                    <Currency value={ lotteryInfo.userBalance } decimals={0} isWei/>
+                    &nbsp;CRUSH
+                  </Typography>
+                </Stack>
+                <Stack direction="row" justifyContent="space-between" px={3} mb={2}>
+                  <Typography>
+                    Total
+                  </Typography>
+                  <Typography color="primary">
+                    <Currency value={ lotteryInfo.ticketPrice.times(values.ticketAmount)} decimals={0} isWei/>
+                    &nbsp;CRUSH
+                  </Typography>
+                </Stack>
+                <Stack spacing={2}>
+                  {
+                    isApproved ? <>
+                      <Button color="primary" background="primary" onClick={submitForm}
+                        disabled={hasError || isNaN(parseInt(values.ticketAmount)) || parseInt(values.ticketAmount) == 0}
+                      >
+                        Choose Tickets
+                      </Button>
+                      <Button color="secondary"
+                        disabled={hasError || isNaN(parseInt(values.ticketAmount)) || parseInt(values.ticketAmount) == 0}
+                        onClick={() => {
+                          setFieldValue('instant', true)
+                          submitForm()
+                        }}
+                      >
+                        Lucky Draw
+                      </Button>
+                    </>
+                    : <Button onClick={() => approve(lotteryContract.address)}>
+                        Approve CRUSH
+                      </Button>
+                  }
+                  <Typography variant="subtitle2" px={2} whiteSpace="pre-line">
+                    &quot;Choose Tickets&quot; Shows you our draw selections and allows you to edit your invader team to customize your perfect team.{'\n'}
+                    &quot;Lucky Draw&quot; chooses your ticket numbers randomly with no duplicates on this draw.{'\n'}
+                    Purchases are final.
+                  </Typography>
+                </Stack>
+              </Form>
+            }}
+          </Formik>
+        </>
+    }
+    {
+      step === 1 && 
+      <>
+        <Typography variant="subtitle1" align="center" fontWeight={600}>
+          Your Tickets
+        </Typography>
+        {tickets.map( (ticketNumber, ticketIndex) => {
+          const ticketDigits = ticketNumber.split('')
+          const selectTicket = () => {
+            setSelectedTicket(ticketIndex)
+            setStep(2)
+          }
+          return <Stack key={`tickets-to-buy-${ticketIndex}`}>
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+              <Typography variant="subtitle2" color="textSecondary">
+                #{ticketIndex+1}
+              </Typography>
+              <IconButton size="small" 
+                onClick={selectTicket}
+                sx={theme => ({ color: theme.palette.blue?.main })}
+              >
+                <EditIcon fontSize="inherit"/>
+              </IconButton>
             </Stack>
-          </Paper>
-        </Stack>
-      })}
+            <Paper
+              sx={ theme => ({
+                backgroundColor: theme.palette.mode == "dark" ? "#0C0E22" : alpha(theme.palette.primary.dark,0.2),
+                borderRadius: 3,
+                px: 2,
+                py: 2,
+              })}
+            >
+              <Stack direction="row">
+                <NumberInvader twoDigits={[ticketDigits[1], ticketDigits[2]]}/>
+                <NumberInvader twoDigits={[ticketDigits[3], ticketDigits[4]]}/>
+                <NumberInvader twoDigits={[ticketDigits[5], ticketDigits[6]]}/>
+              </Stack>
+            </Paper>
+          </Stack>
+        })}
+        <Button color="secondary" onClick={() => setStep(3)} sx={{ mt: 2}}>
+          Get Tickets
+        </Button>
+      </>
+    }
+    {
+      step == 2 && 
+      <>
+      </>
+    }
+    {
+      step == 3 && 
+      <>
+      Thanks
       </>
     }
   </Dialog>
