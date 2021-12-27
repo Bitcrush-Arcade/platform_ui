62d3f121f22a8cc6b05a8f6b1dd34ec32029f7b4
yawgmoth19 <urzasguilt4@gmail.com>
2021-12-27 17:57:41 -0600

    Scroll bar for stack and divider

--- components/lottery/LotteryViews/Current.tsx
+++ components/lottery/LotteryViews/Current.tsx
@@ -1,7 +1,9 @@
+import format from 'date-fns/format'
 // Material
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
 // Libs
 import NumberInvader from 'components/lottery/NumberInvader'
 
@@ -9,6 +11,7 @@ import NumberInvader from 'components/lottery/NumberInvader'
 {/* Current table props */}
 type CurrentViewProps = {
   tickets: Array<string>;
+  nextDrawDate: number;
 }
 
 {/* Current table props and formatting */}
@@ -16,34 +19,37 @@ const Current = (props: CurrentViewProps) => {
 const { tickets } = props
 
 return <>
-  <Typography sx={{pl: 2, pb: 2}} variant="h6" align="center" fontWeight={600}>
+
+  <Typography sx={{p: 2}} variant="h6" align="center" fontWeight={600}>
     Your Tickets
   </Typography>
-  {tickets.map( (ticketNumber, ticketIndex) => {
-    const ticketDigits = ticketNumber.split('')
-    
-    console.log(ticketDigits)
-    return <Stack style={{alignItems: "center"}} justifyContent = "center" direction ="row" key={`tickets-to-buy-${ticketIndex}`}>
-      <Typography variant="h6" sx={{pr: 1}}>
-        #{ticketIndex+1}
-      </Typography>
-      <Paper  
-        sx={ theme => ({
-          backgroundColor: theme.palette.mode == "dark" ? "#0C0E22" : theme.palette.primary.dark,
-          borderRadius: 3,
-          px: 2,
-          py: 2,
-          m: 1,
-        })}
-      >
-        <Stack direction="row">
-          <NumberInvader size="small" twoDigits={[ticketDigits[1], ticketDigits[2]]}/>
-          <NumberInvader size="small" twoDigits={[ticketDigits[3], ticketDigits[4]]}/>
-          <NumberInvader size="small" twoDigits={[ticketDigits[5], ticketDigits[6]]}/>
-        </Stack>
-      </Paper>
-    </Stack>
-  })}
+  <Divider my={2}/>
+  
+  <Stack sx={{ maxHeight: 400, overflowY: 'auto'}}>
+    {tickets.map( (ticketNumber: string, ticketIndex: number) => {
+      const ticketDigits = ticketNumber.split('')
+      return <Stack  justifyContent="center" alignItems="center" direction ="row" key={`tickets-to-buy-${ticketIndex}`}>
+        <Typography variant="h6" sx={{pr: 1}}>
+          #{ticketIndex+1}
+        </Typography>
+        <Paper  
+          sx={ theme => ({
+            backgroundColor: theme.palette.mode == "dark" ? "#0C0E22" : theme.palette.primary.dark,
+            borderRadius: 3,
+            px: 2,
+            py: 2,
+            m: 1,
+          })}
+        >
+          <Stack direction="row">
+            <NumberInvader size="small" twoDigits={[ticketDigits[1], ticketDigits[2]]}/>
+            <NumberInvader size="small" twoDigits={[ticketDigits[3], ticketDigits[4]]}/>
+            <NumberInvader size="small" twoDigits={[ticketDigits[5], ticketDigits[6]]}/>
+          </Stack>
+        </Paper>
+      </Stack>
+    })}
+  </Stack>
 </>
 }
 export default Current  
