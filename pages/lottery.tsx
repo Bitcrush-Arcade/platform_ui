8fe8500449b65ec4e10a829823332ccbde31d31f
jmanywhere <josmfernandez@gmail.com>
2021-12-27 21:33:48 -0600

    show all

--- pages/lottery.tsx
+++ pages/lottery.tsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 // Next
 import Head from 'next/head'
 // Material
 import Grid from '@mui/material/Grid'
-// Bitcrush
+// Bitcrush UI
 import LotteryHistory from 'components/lottery/LotteryHistory'
 import PageContainer from 'components/PageContainer'
 import SummaryCard from 'components/lottery/SummaryCard'
 import TicketBuyModal from 'components/lottery/TicketBuyModal'
+import TicketSelectCard from 'components/lottery/TicketSelectCard'
+// Hooks
+import { useContract } from 'hooks/web3Hooks'
+import { useWeb3React } from '@web3-react/core'
+// Utils & Data
+import { getContracts } from 'data/contracts'
 
-const Trade = () => {
-
-  const [ openBuy, setOpenBuy ] = useState<boolean>(true)
+const Lottery = () => {
+  const [ openBuy, setOpenBuy ] = useState<boolean>(false)
   const toggleOpenBuy = () => setOpenBuy( p => !p )
 
   return <PageContainer>
@@ -19,13 +24,13 @@ const Trade = () => {
       <title>BITCRUSH - Lottery</title>
       <meta name="description" content="The lottery where everyone wins."/>
     </Head>
-    <SummaryCard onBuy={toggleOpenBuy} />
+    <SummaryCard onBuy={toggleOpenBuy}/>
     <Grid container sx={{mt: 4}} justifyContent="space-between">
       <Grid item xs={12} md={6}>
         <LotteryHistory/>
       </Grid>
-      <Grid item>
-        Check winning
+      <Grid item xs={12} md={5}>
+        <TicketSelectCard/>
       </Grid>
     </Grid>
     <TicketBuyModal
@@ -35,4 +40,4 @@ const Trade = () => {
   </PageContainer>
 }
 
-export default Trade
+export default Lottery
