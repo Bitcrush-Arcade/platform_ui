59c800976d55be753e9f77b97d92b9745d372143
jmanywhere <josmfernandez@gmail.com>
2021-12-27 21:33:39 -0600

    data for bonus partner tokens

---
+++ data/partnerTokens.ts
@@ -0,0 +1,7 @@
+export const partnerTokens:{ [tokenAddress: string]: {
+  name: string,
+  img?: string,
+}}= {
+  "0xd23811058eb6e7967d9a00dc3886e75610c4abba": { name: "KNIGHT" },
+  "0x53e562b9b7e5e94b81f10e96ee70ad06df3d2657": { name: "BABYT" },
+}
