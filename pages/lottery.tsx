69236a403b6b22a844bd695957ed80856a88dee5
jmanywhere <josmfernandez@gmail.com>
2021-12-28 22:14:44 -0600

    test custom partner banner

--- pages/lottery.tsx
+++ pages/lottery.tsx
@@ -2,8 +2,10 @@ import { useState, useCallback } from 'react'
 // Next
 import Head from 'next/head'
 // Material
+import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 // Bitcrush UI
+import Card from 'components/basics/Card'
 import LotteryHistory from 'components/lottery/LotteryHistory'
 import PageContainer from 'components/PageContainer'
 import SummaryCard from 'components/lottery/SummaryCard'
@@ -30,7 +32,35 @@ const Lottery = () => {
         <LotteryHistory/>
       </Grid>
       <Grid item xs={12} md={5}>
-        <TicketSelectCard/>
+        <Box
+          sx={theme => ({
+            p: 2,
+            m: 2,
+            position:'relative',
+            backgroundColor: theme.palette.primary.main,
+            maxWidth: 350,
+            height: 80,
+            clipPath: 'polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0)'
+          })}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '2px',
+              bottom: '2px',
+              left: '2px',
+              right: '2px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'black',
+              clipPath: 'polygon(92% 0, 100% 25%, 100% 100%, 8% 100%, 0% 75%, 0 0)'
+            }}
+          >
+            Bonus for this round:
+            5000 KNIGHT pool
+          </Box>
+        </Box>
       </Grid>
     </Grid>
     <TicketBuyModal
