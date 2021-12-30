38e193c4d3f6a252807f10d1fb86aebb86fb11e2
yawgmoth19 <urzasguilt4@gmail.com>
2021-12-29 21:31:59 -0600

    Working on styling and sizes

--- components/lottery/LotteryViews/Current.tsx
+++ components/lottery/LotteryViews/Current.tsx
@@ -21,32 +21,32 @@ const { tickets } = props
 return <>
 
   <Typography sx={{p: 0.5}} variant="h6" align="center" fontWeight={600}>
-    Your Tickets
+    Your Squadron
   </Typography>
   <Divider sx={{my: 2}}/>
   
-  <Stack sx={{ maxHeight: 329, overflowY: 'auto'}}>
+  <Stack sx={{ maxHeight: 353, overflowY: 'auto'}}>
   {
     tickets ? <>
       {tickets.map( (ticketNumber: string, ticketIndex: number) => {
           const ticketDigits = ticketNumber.split('')
-          return <Stack  justifyContent="center" alignItems="center" direction ="row" key={`tickets-to-buy-${ticketIndex}`}>
-            <Typography variant="h6" sx={{pr: 1}}>
+          return <Stack key={`tickets-to-buy-${ticketIndex}`}>
+            <Typography align="left" variant="h6" sx={{pr: 1}}>
               #{ticketIndex+1}
             </Typography>
             <Paper  
               sx={ theme => ({
                 backgroundColor: theme.palette.mode == "dark" ? "#0C0E22" : theme.palette.primary.dark,
                 borderRadius: 3,
-                px: 2,
+                px: 1,
                 py: 2,
                 m: 1,
               })}
             >
-              <Stack direction="row">
-                <NumberInvader size="small" twoDigits={[ticketDigits[1], ticketDigits[2]]}/>
-                <NumberInvader size="small" twoDigits={[ticketDigits[3], ticketDigits[4]]}/>
-                <NumberInvader size="small" twoDigits={[ticketDigits[5], ticketDigits[6]]}/>
+              <Stack direction="row" justifyContent="center">
+                <NumberInvader size="large" twoDigits={[ticketDigits[1], ticketDigits[2]]}/>
+                <NumberInvader size="large" twoDigits={[ticketDigits[3], ticketDigits[4]]}/>
+                <NumberInvader size="large" twoDigits={[ticketDigits[5], ticketDigits[6]]}/>
               </Stack>
             </Paper>
           </Stack>
