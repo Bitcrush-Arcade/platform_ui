92cf5cad7d351ee71ff78d7a4964b81df5e47fc1
jmanywhere <josmfernandez@gmail.com>
2021-12-31 10:51:07 -0600

    disable if no current round

--- components/lottery/LotteryHistory.tsx
+++ components/lottery/LotteryHistory.tsx
@@ -51,7 +51,7 @@ const LotteryHistory = (props: LotteryHistoryProps) => {
                 {/*Tab changer*/}
                 <Tabs value={tabSelected} onChange={selectTab} indicatorColor="secondary" textColor="inherit">
                     <Tab label={<Typography color='white' variant="body1">CURRENT</Typography>}/>
-                    <Tab label={<Typography color='white' variant="body1" >LAST ROUND</Typography>}/>
+                    <Tab label={<Typography color={!currentRound ? 'textSecondary' : 'white'} variant="body1" >LAST ROUND</Typography>} disabled={!currentRound} />
                     <Tab label={<Typography color='white' variant="body1">HISTORY</Typography>}/>
                     
                 </Tabs>
