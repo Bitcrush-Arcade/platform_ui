94f3344bf5a48e074693185c710da04c38c8f95b
jmanywhere <josmfernandez@gmail.com>
2021-12-31 03:18:54 -0600

    added chain in name

--- data/partnerTokens.ts
+++ data/partnerTokens.ts
@@ -2,8 +2,8 @@ export const partnerTokens:{ [tokenAddress: string]: {
   name: string,
   img?: string,
 }}= {
-  "0xd23811058eb6e7967d9a00dc3886e75610c4abba": { name: "KNIGHT" },
+  "0xd23811058eb6e7967d9a00dc3886e75610c4abba": { name: "KNIGHT", img: '/assets/thirdPartyLogos/partners/knightswap-logo.png' },
   "0x53e562b9b7e5e94b81f10e96ee70ad06df3d2657": { name: "BABYT" },
-  "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06": { name: 'CHAIN-TEST'}, // Testnet Chainlink
-  "0x404460C6A5EdE2D891e8297795264fDe62ADBB75": { name: 'CHAIN'},// MainNet ChainLink
+  "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06": { name: 'CHAIN-TEST', img: '/assets/thirdPartyLogos/partners/chainlink-light.png'}, // Testnet Chainlindk
+  "0x404460C6A5EdE2D891e8297795264fDe62ADBB75": { name: 'CHAIN', img: '/assets/thirdPartyLogos/partners/chainlink-light.png'},// MainNet ChainLink
 }
