1ccc91ef9da915e2eccd11fe908fa4d104912ef6
jmanywhere <josmfernandez@gmail.com>
2022-01-04 23:09:45 -0600

    update roundinfo type

--- types/lottery.ts
+++ types/lottery.ts
@@ -1,17 +1,11 @@
 import type BigNumber from 'bignumber.js'
-export type TicketInfo = { ticketNumber: string, claimed: boolean }
+export type TicketInfo = string
 export type RoundInfo = { 
   totalTickets: BigNumber,
   winnerNumber: string,
   userTickets?: Array<TicketInfo>,
   endTime: BigNumber,
-  noMatch: BigNumber,
-  match1: BigNumber,
-  match2: BigNumber,
-  match3: BigNumber,
-  match4: BigNumber,
-  match5: BigNumber,
-  match6: BigNumber,
+  distribution: Array<BigNumber>,
   pool: BigNumber,
   bonusInfo?: {
     bonusToken: string,
