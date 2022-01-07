de01d1c222fe80f1c5b96df61b8f0ff6ac0322fc
jmanywhere <josmfernandez@gmail.com>
2022-01-07 04:43:50 -0600

    added missing head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Document, { Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head,Main, NextScript } from 'next/document'
 import ServerStyleSheets from '@mui/styles/ServerStyleSheets';
 
 class MyDocument extends Document {
@@ -7,7 +7,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        
+        <Head/>
         <body style={{margin: 0}}>
           <Main />
           <NextScript />
