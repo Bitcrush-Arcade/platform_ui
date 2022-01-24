1c689eef3147d1091f26e3fff53b2fc5e39c0270
jmanywhere <josmfernandez@gmail.com>
2022-01-24 16:51:57 -0600

    updated type

--- types/liveWallets.ts
+++ types/liveWallets.ts
@@ -1,9 +1,11 @@
 export type Wallet = {
   status: boolean,
-  walletIcon: any,
+  walletIcon: { alt_title:string, title: string, asset: {_ref:string}},
   walletContract: any,
   tokenName: any,
   symbolToken:string,
   balance?: string,
   walletBalance?: string,
+  timelock?: string, // The time when address was locked in
+  isTimelockActive?: boolean //If the lock has run it's course it returns false
 }
