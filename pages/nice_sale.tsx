be8b41ccde049816f05ffe1e66724b2b01673254
jmanywhere <josmfernandez@gmail.com>
2022-02-14 23:03:14 -0600

    added conditionals

--- pages/nice_sale.tsx
+++ pages/nice_sale.tsx
@@ -1,12 +1,15 @@
-import { useMemo, useState, useEffect, useRef } from 'react'
+import { useMemo, useState, useEffect, useRef, useCallback   } from 'react'
 // Next
 import Head from 'next/head'
 import Image from 'next/image'
 // Other
 import Countdown from 'react-countdown'
 // Icons
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import CancelIcon from '@mui/icons-material/Cancel';
 // Material
 import TextField from '@mui/material/TextField'
+import Tooltip from '@mui/material/Tooltip'
 // Bitcrush UI
 import PageContainer from 'components/PageContainer'
 import useCoin from 'hooks/useCoin'
@@ -22,17 +25,19 @@ import { toWei } from 'web3-utils'
 const NiceSale = () => {
   const [totalRaised, setTotalRaised] = useState<BigNumber>( new BigNumber(0) )
   const [fillPercent, setFillPercent] = useState<number>(50);
+  const [nftId, setNftId] = useState<string>("")
   const [buyAmount, setBuyAmount] = useState<number>(0)
+  const [prequalified, setPrequalified] = useState<boolean>(false)
   const {account, chainId} = useWeb3React()
 
-  const [presaleData, setPresaleData] = useState({})
+  const [presaleData, setPresaleData] = useState<{whitelisted: number}>({whitelisted: 0})
 
   const { abi: psAbi, address: presaleContract} = getContracts('presale', chainId)
   const busdContract = useMemo(()=> {
     switch(chainId){
-      case 56: //BSC Mainnet
+      case 56: //BSC Mainnet actual BUSD contract
         return "0xe9e7cea3dedca5984780bafc599bd69add087d56"
-      case 97: // BSC Testnet 
+      case 97: // BSC Testnet using same as CRUSH
         return "0xa3ca5df2938126bae7c0df74d3132b5f72bda0b6"
       default: 
         return null
@@ -41,11 +46,28 @@ const NiceSale = () => {
 
   const { approve, getApproved, isApproved } = useCoin(busdContract || undefined)
   const { methods: psMethod } = useContract(psAbi, presaleContract)
+
+  const getPresaleData = useCallback(async() => {
+    if(!account) return
+    
+  },[setPresaleData, psMethod, account])
+
+  const checkPreQ = useCallback( async () => {
+    if(!account || !psMethod) return
+    const isPreQualified = await psMethod.qualify().call({from: account})
+    console.log({isPreQualified})
+    setPrequalified(Boolean(isPreQualified))
+  },[account, psMethod, setPrequalified])
   
   useEffect( () => {
     if(!busdContract || !account || !presaleContract) return
     getApproved(presaleContract)
   },[busdContract, account, presaleContract, getApproved])
+
+  useEffect( () => {
+    if(!account || !psMethod) return
+    checkPreQ()
+  },[account, checkPreQ, psMethod])
 
   const approveBUSD = () => {
     approve(presaleContract, new BigNumber(5000).times(10^18))
@@ -54,8 +76,14 @@ const NiceSale = () => {
   const inputRef=useRef<HTMLInputElement>(null)
 
   const buyTokens = () => {
-    const tokenAmount = inputRef.current?.value || 0
-    console.log('input Value', tokenAmount)
+    console.log({buyAmount})
+  }
+1
+  const whitelist = () => {
+    const tokenId = parseInt(inputRef.current?.value || '0')
+    console.log({tokenId})
+    if(isNaN(tokenId) || tokenId == 0) return
+
   }
   return <>
     <Head>
@@ -66,14 +94,16 @@ const NiceSale = () => {
     <PageContainer background='galactic'>
       <div className="flex items-center justify-center px-2">
         {/* PRESALE CARD CONTAINER */}
-        <div className="flex-grow border-2 border-secondary rounded-[32px] p-5 bg-paper-bg inner-glow-secondary max-w-[450px] text-white">
-          <h2 className="text-right font-bold text-xl font-zeb tracking-widest">
-            NICE Presale
-          </h2>
-          <div className="flex flex-row items-center">
-            <Image src={"/assets/glowy.png"} width={75} height={75} alt="Invaderverse Logo"/>
-            <p className="pl-3 flex-grow text-justify text-sm">
-              $NICE is the official currency of the Invaderverse.
+        <div className="flex-grow border-2 border-secondary rounded-[32px] p-5 bg-paper-bg inner-glow-secondary max-w-[450px] text-white mt-7">
+          <div className="flex flex-row items-center justify-between">
+            <Image src={"/assets/glowy.png"} width={75} height={75} alt="Invaderverse Logo" className='min-w-[75px]'/>
+            <h2 className="text-right font-bold text-xl font-zeb tracking-widest">
+              NICE Presale
+            </h2>
+          </div>
+          <div className="py-2">
+            <p className="text-justify text-sm">
+              $NICE is the official currency of the Invaderverse. Presale is by whitelist only, you can whitelist yourself if you meet the requirements listed below.
             </p>
           </div>
           {/* Fill bar */}
@@ -91,6 +121,63 @@ const NiceSale = () => {
             <span>
               {totalRaised.toString()} / 125,000
             </span>
+          </div>
+          <div className='flex flex-row justify-between px-2'>
+            <p>
+              Do I qualify?
+            </p>
+            {
+              prequalified 
+              ?
+                <Tooltip title="All requirements met">
+                  <CheckCircleIcon color="secondary"/>
+                </Tooltip>
+              :
+                <Tooltip title={<p className="whitespace-pre-line">
+                    Check that you:{"\n"}
+                    * Own a Crush God NFT{"\n"}
+                    * Have at least 10k $CRUSH staked in Auto Bitcrush V2{"\n"}
+                  </p>
+                }>
+                  <CancelIcon color="error"/>
+                </Tooltip>
+            }
+          </div>
+          <div className='flex flex-col md:flex-row justify-between items-center px-2 py-2'>
+            <p className='mb-2'>
+              Am I whitelisted?
+            </p>
+            {
+              presaleData.whitelisted > 0
+              ?
+                <Tooltip title={`Whitelisted with NFT ID ${presaleData.whitelisted}`}>
+                  <CheckCircleIcon color="secondary"/>
+                </Tooltip>
+              :
+                <div className='flex items-center gap-x-1'>
+                  <TextField
+                    size="small"
+                    value={nftId}
+                    onChange={(e) => {
+                      const selectedId = parseInt(e.target.value)
+                      if(e.target.value == ""){
+                        setNftId("")
+                        return
+                      }
+                      if(isNaN(selectedId)) return
+                      setNftId(e.target.value)
+                    }}
+                    label="NFT ID"
+                    className="max-w-[80px]"
+                  />
+                  <button disabled={isNaN(parseInt(nftId))}
+                    onClick={whitelist}
+                    className="border-2 border-secondary px-3 py-1 inner-glow-secondary text-xs rounded-full hover:bg-secondary hover:text-black max-w-[8em] disabled:opacity-60 disabled:hover:bg-transparent disabled:hover:text-white"
+                  >
+                    WHITELIST ME
+                  </button>
+                </div>
+            }
           </div>
           {/* Approve and BUY */}
           <div className='flex flex-row justify-center mt-4'>
@@ -146,7 +233,7 @@ const NiceSale = () => {
             <span>{currencyFormat(125000, {decimalsToShow: 0})}</span>
           </div>
           <div className='text-lg px-1 flex flex-row justify-between text-[0.9em]'>
-            <span>Token Price (NICE/BUSD)</span>
+            <span>Presale Price (NICE/BUSD)</span>
             <span>0.00470</span>
           </div>
           <div className='text-lg px-1 flex flex-row justify-between text-[0.9em]'>
