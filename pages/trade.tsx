1f0de1e8e47b109a1790862bc5f29753a003cf75
Yawgmoth19 <urzasguilt4@gmail.com>
2022-02-15 16:03:13 -0600

    Started drawing bridge

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -1,19 +1,53 @@
 // Next
 import Head from 'next/head'
 // Material
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Card from 'components/basics/Card';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography'
+
 // Bitcrush
 import PageContainer from 'components/PageContainer'
 
 const Trade = () => {
   return <PageContainer>
     <Head>
-      <title>BITCRUSH - TRADE (Coming Soon)</title>
-      <meta name="description" content="Trading Coming Soon"/>
+      <title>BITCRUSH - BRIDGE</title>
+      <meta name="description" content="Cross Chain Bridge"/>
     </Head>
-    <Typography variant="h1" align="center">
-      TRADING COMING SOON
+    <Typography variant="h2" align="center">
+      <text>
+        Welcome to the  
+      </text>
     </Typography>
+
+    <h2 className="text-center font-bold text-xl font-zeb tracking-widest">
+    Intergalactic Bridge
+    </h2>
+    
+    <Card background="light" shadow="secondary" sx={{p: 3}}>
+      <TextField
+            required
+            id="outlined-required"
+            label="From:"
+            defaultValue="0.0"
+            type="number"
+          />
+          
+    </Card>
+    
+    <ArrowDownwardIcon color="secondary" sx={{fontSize: 40, bottom: 0, position: 'relative', mx: 0.5}} />
+
+    <Card background="light" shadow="secondary" sx={{p: 3}}>
+      <TextField
+            required
+            id="outlined-required"
+            label="To:"
+            defaultValue="0.0"
+            type="number"
+          />
+    </Card>
+    
   </PageContainer>
 }
 
