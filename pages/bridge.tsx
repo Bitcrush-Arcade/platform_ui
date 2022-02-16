e5df90d568e93429bbb53d997a16f786b3a28dbb
Yawgmoth19 <urzasguilt4@gmail.com>
2022-02-16 02:25:53 -0600

    Card component

--- pages/bridge.tsx
+++ pages/bridge.tsx
@@ -9,7 +9,6 @@ import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 // Bitcrush
 import PageContainer from 'components/PageContainer'
 import BridgeCard from 'components/bridge/BridgeCard';
-import BridgeOptions from 'components/bridge/BridgeOptions';
 
 const Trade = () => {
   return <PageContainer>
@@ -30,51 +29,7 @@ const Trade = () => {
 
     <div className="grid rows-3 gap-4 justify-items-center content-center">
 
-      {/* Main chain CARD CONTAINER */}
-      <div className="grid grid-rows-1 grid-cols-2 gap-2 justify-items-center items-center border-2 border-primary rounded-[32px] p-10 bg-paper-bg inner-glow-primary max-w-[900px]">
-        
-        <TextField
-        required
-        id="outlined-required"
-        label="From"
-        defaultValue="0.0"
-        inputProps={{style: {fontSize: 25}}}
-        />
-
-        {/* Button column */}   
-        <div className="grid grid-cols-2 place-content-between gap-4">
-          <button 
-            className="grid place-content-between items-center grid-cols-2 grid-rows-1 gap-10 border-2 border-secondary px-4 py-2 inner-glow-secondary text-xs rounded-full hover:bg-secondary hover:text-black max-w-[15em] disabled:opacity-60 disabled:hover:bg-transparent disabled:hover:text-white"
-          >
-            <div>
-              TOKEN
-            </div>
-            {/* Circle arrow down icon */}  
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-              <path 
-              fillRule="evenodd" 
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" 
-              clipRule="evenodd" 
-              />
-            </svg>
-          </button>
-
-          <button 
-            className="grid place-content-between items-center grid-cols-2 grid-rows-1 gap-10 border-2 border-secondary px-4 py-2 inner-glow-secondary text-xs rounded-full hover:bg-secondary hover:text-black max-w-[15em] disabled:opacity-60 disabled:hover:bg-transparent disabled:hover:text-white"
-          >
-            <div>
-              CHAIN
-            </div>  
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-              <path 
-              fillRule="evenodd" 
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" 
-              clipRule="evenodd" 
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
+      <BridgeCard fromTo="from" borderColor="primary" buttonColor="secondary"/>
 
       {/* Arrow down icon */}  
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -82,6 +37,7 @@ const Trade = () => {
       </svg>
 
       {/* Side chain CARD CONTAINER */}
+      <BridgeCard fromTo="to" borderColor="secondary" buttonColor="primary"/>
 
     </div>
 
