263f2c49bd3c934a64e974169abc880f113a4da7
Yawgmoth19 <urzasguilt4@gmail.com>
2022-02-17 23:14:34 -0600

    Currency selection

--- tw/bridge/BridgeCard.tsx
+++ tw/bridge/BridgeCard.tsx
@@ -1,5 +1,7 @@
-import { truncate } from "lodash"
-
+import { useState, useCallback } from 'react'
+// Bitcrush UI
+// Hooks
+import { useAuthContext } from 'hooks/contextHooks'
 // Props
 type BridgeCardProps = {
   account: string;
@@ -9,13 +11,17 @@ type BridgeCardProps = {
 
 const BridgeCard = (props: BridgeCardProps) => {
 
-  const { account, amount, walletBalance } = props
-  let tokenStatus=true
-  const tokenToggle= () => {
-    tokenStatus=!tokenStatus
-  }
+  const { amount, walletBalance } = props
+
+  const { login, account, chainId } = useAuthContext()
+  const [selectedToken, setSelectedToken] = useState<boolean>(false)
+
   
-  return <div className="flex flex-col gap-10 border-2 border-secondary inner-glow-secondary bg-paper-bg px-11 py-14 rounded-[32px] max-w-[500px]">
+  const tokenToggle= useCallback(() => {
+    setSelectedToken( p => !p)
+  },[setSelectedToken])
+  
+  return <div className="flex flex-col gap-10 border-2 border-secondary inner-glow-secondary bg-paper-bg p-10 rounded-[32px] max-w-[500px]">
           
           <div className="flex flex-row justify-between">
             <div>
@@ -31,23 +37,38 @@ const BridgeCard = (props: BridgeCardProps) => {
 
           <div className="flex flex-col items-center gap-2">
             {
-              tokenStatus
+              selectedToken
                 ?
-            <button onClick={tokenToggle} className="flex flex-row items-center gap-2 border-2 border-primary inner-glow-primary px-6 py-4 text-xs rounded-full hover:bg-primary hover:text-black">
-            NICE
-            </button>
+
+            <div className="grid grid-cols-3 justify-items-stretch items-center gap-2">
+              <h2 className="justify-self-end text-s">
+                NICE
+              </h2>
+              <button onClick={tokenToggle} className="justify-self-center flex flex-row border-2 border-primary rounded-[32px] w-20 p-1 ">
+                <div className="flex flex-row border-4 border-primary bg-primary rounded-[32px] basis-1/2 px-2 h-6 w-10"/>
+              </button>
+              <h2 className='text-xs text-slate-500'>
+                CRUSH
+              </h2>
+            </div>
             :
-            <button onClick={tokenToggle} className="flex flex-row items-center gap-2 border-2 border-primary inner-glow-primary px-6 py-4 text-xs rounded-full hover:bg-primary hover:text-black">
-            CRUSH
-            </button>
+            <div className="grid grid-cols-3 justify-items-stretch items-center gap-2">
+              <h2 className="text-xs justify-self-end text-slate-500">
+                NICE
+              </h2>
+              <button onClick={tokenToggle} className="justify-self-center flex flex-row justify-end border-2 border-primary rounded-[32px] w-20 p-1">
+                <div className="basis-1/2 flex flex-row border-4 border-primary bg-primary rounded-[32px] px-2 h-6 w-10"/>
+              </button>
+              <h2 className="text-s">
+                CRUSH
+              </h2>
+            </div>
 
             }
             <div>
             TEXTFIELD FOR AMOUNT
             </div>
-          </div>
-
-          <div className="flex flex-row justify-center">
+            
             {
               account
                 ?
@@ -58,16 +79,17 @@ const BridgeCard = (props: BridgeCardProps) => {
                 BRIDGE TOKENS
               </button>
               :
-              <button className="flex flex-row items-center gap-2 border-2 border-primary inner-glow-primary px-6 py-4 text-xs rounded-full hover:bg-primary hover:text-black">
+              <button className="flex flex-row items-center gap-2 border-2 border-primary inner-glow-primary px-6 py-4 text-xs rounded-full hover:bg-primary hover:text-black" onClick={login}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                 </svg>
                 CONNECT TO WALLET
               </button>
             }
+           
           </div>
 
-          <div className="flex border-2 border-primary rounded-[32px] justify-center mx-2">
+          <div className="flex border-2 border-primary rounded-[32px] justify-center mx-2 p-4">
             <h2 className="text-center text-m whitespace-pre-line">
               CAPTION DATA{'\n'}
               <span className="text-s">
