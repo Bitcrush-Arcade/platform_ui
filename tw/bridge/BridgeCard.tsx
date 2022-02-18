bdebbcdc3293f2304ddd922be973350ffb7d0ad5
Yawgmoth19 <urzasguilt4@gmail.com>
2022-02-18 03:15:06 -0600

    Main layout finished

--- tw/bridge/BridgeCard.tsx
+++ tw/bridge/BridgeCard.tsx
@@ -21,19 +21,26 @@ const BridgeCard = (props: BridgeCardProps) => {
     setSelectedToken( p => !p)
   },[setSelectedToken])
   
-  return <div className="flex flex-col gap-10 border-2 border-secondary inner-glow-secondary bg-paper-bg p-10 rounded-[32px] max-w-[500px]">
-          <h2 className="text-primary self-center">
+  return <div className="flex flex-col gap-10 border-2 border-secondary inner-glow-secondary bg-paper-bg px-10 pt-7 pb-10 rounded-[32px] max-w-[500px]">
+
+          <div className="grid grid-col grid-cols-5 grid-rows-2 justify-items-center items-center">
+          <h2 className="text-primary col-start-1 col-span-5 self-center">
               Select chains
           </h2>
-          <div className="grid grid-col grid-cols-3 justify-items-center">
-            <div>
-            From 
+            <div className="col-start-1 col-span-2 grid justify-items-center">
+              <h2 className="text-s">
+              FROM 
+              </h2>
+              <ChainSelector/>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 col-start-3 col-span-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
             </svg>
-            <div>
-            To 
+            <div className="col-start-4 col-span-2 grid justify-items-center">
+              <h2 className="text-s">
+                TO 
+              </h2>
+              <ChainSelector/> 
             </div>
           </div>
 
@@ -77,7 +84,7 @@ const BridgeCard = (props: BridgeCardProps) => {
                 Amount to bridge
               </h2>
               <input
-                type="text"
+                type="number"
                 className="
                   form-control
                   block
@@ -93,7 +100,7 @@ const BridgeCard = (props: BridgeCardProps) => {
                   transition
                   ease-in-out
                   m-0
-                  focus:text-white focus:bg-paper-bg focus:outline-none
+                  focus:text-white focus:bg-paper-bg focus:outline-none 
                 "
                 id="coinAmount"
                 placeholder="0.0"
@@ -133,10 +140,149 @@ const BridgeCard = (props: BridgeCardProps) => {
           </div>
     </div>
 }
+
 export default BridgeCard
 
 type SelectorProps = {
   onChange:(chainToSelect: string) => void,
   currentChain: string
+  defaultChain: string
 }
-const ChainSelector = (props: SelectorProps)=>{}
+const ChainSelector = (props: SelectorProps) => {
+  
+  return <div className="flex justify-center">
+    <div>
+      <div className="dropdown relative">
+        <button
+          className="
+            dropdown-toggle
+            px-6
+            py-2.5
+            bg-paper-bg
+            border-2 border-secondary 
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            transition
+            duration-150
+            ease-in-out
+            flex
+            items-center
+            whitespace-nowrap
+          "
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          DEFAULT CHAIN
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="caret-down"
+            className="w-2 ml-2"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path
+              fill="currentColor"
+              d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+            ></path>
+          </svg>
+        </button>
+        <ul
+          className="
+            dropdown-menu
+            min-w-max
+            absolute
+            hidden
+            bg-white
+            text-base
+            z-50
+            float-left
+            py-2
+            list-none
+            text-left
+            rounded-lg
+            shadow-lg
+            mt-1
+            hidden
+            m-0
+            bg-clip-padding
+            border-none
+          "
+          aria-labelledby="dropdownMenuButton1"
+        >
+          <li>
+            <a
+              className="
+                dropdown-item
+                text-sm
+                py-2
+                px-4
+                font-normal
+                block
+                w-full
+                whitespace-nowrap
+                bg-transparent
+                text-gray-700
+                hover:bg-gray-100
+              "
+              href="#"
+              >
+                Action
+            </a>
+          </li>
+          <li>
+            <a
+              className="
+                dropdown-item
+                text-sm
+                py-2
+                px-4
+                font-normal
+                block
+                w-full
+                whitespace-nowrap
+                bg-transparent
+                text-gray-700
+                hover:bg-gray-100
+              "
+              href="#"
+              >
+                Another action
+            </a>
+          </li>
+          <li>
+            <a
+              className="
+                dropdown-item
+                text-sm
+                py-2
+                px-4
+                font-normal
+                block
+                w-full
+                whitespace-nowrap
+                bg-transparent
+                text-gray-700
+                hover:bg-gray-100
+              "
+              href="#"
+            >
+            Something else here
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+}
+
