5cf7d57a0bef14539bf30e35a7dc818f8508ea74
Yawgmoth19 <urzasguilt4@gmail.com>
2022-02-22 01:40:53 -0600

    Card

--- pages/farms.tsx
+++ pages/farms.tsx
@@ -1,7 +1,7 @@
 // Next
 import Head from 'next/head';
 import PageContainer from 'components/PageContainer';
-
+import FarmCard from 'tw/farms/FarmCard';
 
 
 const Farm = () => {
@@ -14,7 +14,7 @@ const Farm = () => {
     <h2 className="text-center text-6xl whitespace-pre-line font-zeb">
         Warp Farms
     </h2>
-    
+    <FarmCard/>
   </PageContainer>
 }
 
