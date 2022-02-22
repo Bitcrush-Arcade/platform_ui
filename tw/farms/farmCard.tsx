49d750f05e281f5efa12879d89dbb10581cd5919
Yawgmoth19 <urzasguilt4@gmail.com>
2022-02-22 01:34:52 -0600

    Farm Card

---
+++ tw/farms/farmCard.tsx
@@ -0,0 +1,8 @@
+const FarmCard = () => {
+
+  return <div className="flex flex-col gap-10 border-2 border-secondary inner-glow-secondary bg-paper-bg px-8 pt-1 pb-10 rounded-[32px] max-w-[500px] text-white">
+    HELLO WORLD
+  </div>
+
+}
+export default FarmCard 
