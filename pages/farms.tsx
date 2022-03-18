d0a743a240847c5d76b5e33fdb072123945a69d1
Yawgmoth19 <urzasguilt4@gmail.com>
2022-03-18 02:05:24 -0600

    Warp farms to stellar farms

--- pages/farms.tsx
+++ pages/farms.tsx
@@ -14,7 +14,7 @@ const Farms = () => {
       <meta name="description" content="BITCRUSH Farms" />
     </Head>
     <h2 className="text-center text-6xl whitespace-pre-line font-zeb">
-      Warp Farms
+      Stellar Farms
     </h2>
     <div className="flex justify-center mt-9">
       <div className="flex flex-wrap gap-x-6 gap-y-8 justify-center lg:justify-evenly max-w-[61rem]">
