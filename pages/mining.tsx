ddd4c74b99d732e5c9d7f9516ebfd73bc3cbdb9f
Yawgmoth19 <urzasguilt4@gmail.com>
2022-03-28 14:17:37 -0600

    Compound card import

--- pages/mining.tsx
+++ pages/mining.tsx
@@ -9,7 +9,7 @@ import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Switch from '@mui/material/Switch'
 // Bitcrush
-import CompoundingCardv2 from 'components/pools/CompoundingCardv2'
+import CompoundingCardv3 from 'tw/mining/CompoundingCardv3'
 import PageContainer from 'components/PageContainer'
 import PoolCard from 'components/pools/PoolCard'
 import PoolCardv2 from 'components/pools/PoolCardv2'
@@ -24,21 +24,21 @@ const Mining = () => {
   const { chainId } = useWeb3React()
 
   const [showInactive, setShowInactive] = useState<boolean>(false)
-  const toggleInactive = () => setShowInactive( p => !p )
+  const toggleInactive = () => setShowInactive(p => !p)
 
-  const firstPool = useMemo( () => getContracts('singleAsset', chainId ), [chainId])
-  const prevPool = useMemo( () => getContracts('prevStaking2', chainId ), [chainId])
-  const token = useMemo( () => getContracts('crushToken', chainId ), [chainId])
+  const firstPool = useMemo(() => getContracts('singleAsset', chainId), [chainId])
+  const prevPool = useMemo(() => getContracts('prevStaking2', chainId), [chainId])
+  const token = useMemo(() => getContracts('crushToken', chainId), [chainId])
 
   return <PageContainer background="galactic">
     <Head>
       <title>BITCRUSH - MINING</title>
-      <meta name="description" content="Mine CRUSH to your heart's content. Keep a look for more Pools to stake on soon"/>
+      <meta name="description" content="Mine CRUSH to your heart's content. Keep a look for more Pools to stake on soon" />
     </Head>
     <Grid container justifyContent="space-evenly"
-      sx={ theme => ({
+      sx={theme => ({
         mt: 0,
-        [theme.breakpoints.down('md')]:{
+        [theme.breakpoints.down('md')]: {
           mt: theme.spacing(4)
         }
       })}
@@ -54,14 +54,15 @@ const Mining = () => {
           All you need to do it press the Claim button, and watch the rewards compound for everyone staked in the pool.`}
         />
       </Grid>
-      <Grid item sx={{ pt: { xs: 4, md: 0 }}}>
-        <CompoundingCardv2/>
+      <Grid item sx={{ pt: { xs: 4, md: 0 } }}>
+        <CompoundingCardv3 />
+
       </Grid>
       <Grid item xs={12} sx={{ pt: 4 }}>
-        <BankPool/>
+        <BankPool />
       </Grid>
     </Grid>
-    <Grid container justifyContent="space-evenly" className={ css.section }>
+    <Grid container justifyContent="space-evenly" className={css.section}>
       <Grid item xs={10} sm={8} md={6}>
         <Descriptor
           title="More Pools"
@@ -69,28 +70,28 @@ const Mining = () => {
           No risk pools`}
         />
       </Grid>
-      <Grid item style={{width: 215}}/>
+      <Grid item style={{ width: 215 }} />
     </Grid>
-    <Grid container justifyContent="space-evenly" className={ css.section }>
+    <Grid container justifyContent="space-evenly" className={css.section}>
       <Grid item>
-        <PoolCard disabled abi={firstPool.abi} contractAddress={firstPool.address} tokenAbi={token.abi} tokenAddress={token.address} infoText="No fees! - Crush It!"/>
+        <PoolCard disabled abi={firstPool.abi} contractAddress={firstPool.address} tokenAbi={token.abi} tokenAddress={token.address} infoText="No fees! - Crush It!" />
       </Grid>
     </Grid>
-    <Grid container justifyContent="space-evenly" className={ css.section }>
+    <Grid container justifyContent="space-evenly" className={css.section}>
       <Grid item xs={10} sm={8} md={6}>
         <Descriptor
           title="Inactive Pools"
           description={<>
-          {`Empty pools, nothing to do except withdraw.\n`}
-            Hide <Switch value={showInactive} onClick={toggleInactive}/> Show
+            {`Empty pools, nothing to do except withdraw.\n`}
+            Hide <Switch value={showInactive} onClick={toggleInactive} /> Show
           </>}
         />
       </Grid>
-      <Grid item style={{width: 215}}/>
+      <Grid item style={{ width: 215 }} />
     </Grid>
     {
       showInactive &&
-      <Grid container justifyContent="center" className={ css.section }>
+      <Grid container justifyContent="center" className={css.section}>
         <Grid item>
           <PoolCardv2
             abi={prevPool.abi}
@@ -106,23 +107,23 @@ const Mining = () => {
 
 export default Mining
 
-const useStyles = makeStyles<Theme, {}>( (theme) => createStyles({
-  
-  section:{
+const useStyles = makeStyles<Theme, {}>((theme) => createStyles({
+
+  section: {
     marginTop: theme.spacing(4),
     marginBottom: theme.spacing(4)
   },
-  
+
 }))
 
-const useDescriptorStyles = makeStyles<Theme, {}>( (theme) => createStyles({
-  textContainer:{
+const useDescriptorStyles = makeStyles<Theme, {}>((theme) => createStyles({
+  textContainer: {
     borderLeftColor: theme.palette.primary.main,
     borderLeftStyle: 'solid',
-    borderLeftWidth: theme.spacing(0.5) ,
+    borderLeftWidth: theme.spacing(0.5),
     paddingLeft: theme.spacing(3)
   },
-  paragraph:{
+  paragraph: {
     whiteSpace: 'pre-line',
     lineHeight: '150%'
   },
@@ -130,12 +131,12 @@ const useDescriptorStyles = makeStyles<Theme, {}>( (theme) => createStyles({
 
 const Descriptor = (props: { title: string, description: React.ReactNode }) => {
   const css = useDescriptorStyles({})
-  return <div className={ css.textContainer }>
+  return <div className={css.textContainer}>
     <Typography variant="h4" component="h1" paragraph>
       {props.title}
     </Typography>
-    <Typography variant="body2" className={ css.paragraph }>
+    <Typography variant="body2" className={css.paragraph}>
       {props.description}
     </Typography>
-</div>
+  </div>
 }
